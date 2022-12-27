3e147d99ba5a37b7cb1de18d0df3a80bf23bb175
chetentshering <tsheringcheten93@gmaail.com>
2022-12-27 12:02:12 +0600

    setup

--- hrms/setup.py
+++ hrms/setup.py
@@ -16,7 +16,7 @@ def after_install():
 	add_non_standard_user_types()
 	set_single_defaults()
 	frappe.db.commit()
-	run_post_install_patches()
+	#run_post_install_patches()
 	click.secho("Thank you for installing Frappe HR!", fg="green")
 
 
@@ -634,7 +634,7 @@ def get_post_install_patches():
 		"erpnext.patches.v13_0.update_employee_advance_status",
 		"erpnext.patches.v13_0.update_expense_claim_status_for_paid_advances",
 		"erpnext.patches.v14_0.delete_employee_transfer_property_doctype",
-		"erpnext.patches.v13_0.set_payroll_entry_status",
+		#"erpnext.patches.v13_0.set_payroll_entry_status",
 		# HRMS
 		"create_country_fixtures",
 	)
