45680f3abbdb4fdc79757f7094ead7e8a6425e4b
chetentshering <tsheringcheten93@gmaail.com>
2022-12-27 11:40:52 +0600

    hrms setup

--- hrms/patches/post_install/set_payroll_entry_status.py
+++ hrms/patches/post_install/set_payroll_entry_status.py
@@ -2,17 +2,18 @@ import frappe
 from frappe.query_builder import Case
 
 
-# def execute():
-# 	PayrollEntry = frappe.qb.DocType("Payroll Entry")
+def execute():
+    return 
+	PayrollEntry = frappe.qb.DocType("Payroll Entry")
 
-# 	(
-# 		frappe.qb.update(PayrollEntry)
-# 		.set(
-# 			"status",
-# 			Case()
-# 			.when(PayrollEntry.docstatus == 0, "Draft")
-# 			.when(PayrollEntry.docstatus == 1, "Submitted")
-# 			.else_("Cancelled"),
-# 		)
-# 		.where((PayrollEntry.status.notin(["Queued", "Failed"])))
-# 	).run()
+	(
+		frappe.qb.update(PayrollEntry)
+		.set(
+			"status",
+			Case()
+			.when(PayrollEntry.docstatus == 0, "Draft")
+			.when(PayrollEntry.docstatus == 1, "Submitted")
+			.else_("Cancelled"),
+		)
+		.where((PayrollEntry.status.notin(["Queued", "Failed"])))
+	).run()
