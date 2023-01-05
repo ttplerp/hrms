b8845b707f70cdebaa400ee916eeeba6f98e4aa5
ttplerp <erp.ttpl@gmail.com>
2023-01-05 12:56:12 +0600

    Financial Instituion Branch

--- hrms/hr/doctype/financial_institution_branch/financial_institution_branch.py
+++ hrms/hr/doctype/financial_institution_branch/financial_institution_branch.py
@@ -1,8 +1,9 @@
 # Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class FinancialInstitutionBranch(Document):
-	pass
+	def autoname(self):
+		self.name = " - ".join([self.branch_name,str(self.financial_institution).strip()])
