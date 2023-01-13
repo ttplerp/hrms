160886606e10f7e51d1c8ca763a8c3ce6ab3b8a1
chetentshering <tsheringcheten93@gmaail.com>
2023-01-13 10:47:56 +0600

    monthly salary register

--- hrms/payroll/report/monthly_salary_register/monthly_salary_register.js
+++ hrms/payroll/report/monthly_salary_register/monthly_salary_register.js
@@ -4,6 +4,32 @@
 
 frappe.query_reports["Monthly Salary Register"] = {
 	"filters": [
-
+		{
+			"fieldname": "fiscal_year",
+			"label": __("Fiscal Year"),
+			"fieldtype": "Link",
+			"options": "Fiscal Year",
+			"default": frappe.sys_defaults.fiscal_year,
+		},
+		{
+			"fieldname": "month",
+			"label": __("Month"),
+			"fieldtype": "Select",
+			"options": "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec",
+			"default": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"][frappe.datetime.str_to_obj(frappe.datetime.get_today()).getMonth()],
+		},
+		{
+			"fieldname": "employee",
+			"label": __("Employee"),
+			"fieldtype": "Link",
+			"options": "Employee",
+		},
+		{
+			"fieldname": "company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company")
+		},
 	]
 };
