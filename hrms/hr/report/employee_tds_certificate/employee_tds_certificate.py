55380e81f4dcbeba6f4b55413e7c824fa70742e1
birendra-bit <bhujelbirendra3@gmail.com>
2023-03-09 11:00:36 +0600

    Employee tds certificate modified as it was not working in old format

--- hrms/hr/report/employee_tds_certificate/employee_tds_certificate.py
+++ hrms/hr/report/employee_tds_certificate/employee_tds_certificate.py
@@ -20,13 +20,13 @@ def get_data( filters=None):
 	(select b.amount from `tabSalary Detail` b where salary_component = 'Basic Pay' and b.parent = a.name) as basic_pay,
 	(select b.amount from `tabSalary Detail` b where salary_component = 'Salary Tax' and b.parent = a.name) as tds ,
 	(select b.amount from `tabSalary Detail` b where salary_component = 'PF' and b.parent = a.name) as nppf ,
-	ifnull((select b.amount from `tabSalary Detail` b where salary_component = 'Group Insurance Scheme' and b.parent = a.name),0) as gis ,
+	ifnull((select b.amount from `tabSalary Detail` b where salary_component = 'GIS' and b.parent = a.name),0) as gis ,
 	(select b.amount from `tabSalary Detail` b where salary_component = 'Communication Allowance' and b.parent = a.name) as comm_all ,
 	(select b.amount from `tabSalary Detail` b where salary_component = 'Health Contribution' and b.parent = a.name) as health,
-	r.receipt_number, DATE_FORMAT(r.receipt_date, '%d-%m-%Y') AS receipt_date
-	 from `tabSalary Slip` a, `tabTDS Receipt Entry` r
-	 where a.fiscal_year = r.fiscal_year and a.month = r.month and a.docstatus = 1 
-	 and r.purpose = 'Employee Salary' and a.fiscal_year = """ + str(filters.fiscal_year)
+	r.receipt_number, r.receipt_date, r.posting_date
+	from `tabSalary Slip` a, `tabTDS Receipt Entry` r
+	where a.fiscal_year = r.fiscal_year and a.docstatus = 1 
+	and r.purpose = 'Employee Salary' and a.fiscal_year = """ + str(filters.fiscal_year)
 
 	if filters.employee:
 		salary = salary + " AND a.employee = \'" + str(filters.employee) + "\'"
@@ -34,25 +34,26 @@ def get_data( filters=None):
 	salary+=" order by r.receipt_date asc;"
 	datas = frappe.db.sql(salary, as_dict=True)
 	for d in datas:
-		row = [get_month(d.month)+"-"+d.fyear, 
-			  "Salary", 
-			  d.basic_pay, 
-			  round(flt(d.gross_pay) - flt(d.basic_pay) - (flt(d.comm_all) / 2), 2), 
-			  round(flt(d.gross_pay)-(flt(d.comm_all) / 2),2), 
-			  d.nppf,
-			  d.gis,
-			  flt(d.nppf)+flt(d.gis), 
-			  flt(d.gross_pay) - flt(d.nppf) - flt(d.gis) - (flt(d.comm_all) / 2), 
-			  d.tds if d.tds else 0, 
-			  d.health,
-			  d.receipt_number, 
-			  d.receipt_date,
-			  ""]
+		row = {
+			"month-fyear":get_month(d.month)+"-"+d.fyear, 
+			"type":"Salary", 
+			"basic":d.basic_pay, 
+			"others":round(flt(d.gross_pay) - flt(d.basic_pay) - (flt(d.comm_all) / 2), 2), 
+			"total":round(flt(d.gross_pay)-(flt(d.comm_all) / 2),2), 
+			"pf":d.nppf,
+			"gis":d.gis,
+			"totalPfGis":flt(d.nppf)+flt(d.gis), 
+			"taxable":flt(d.gross_pay) - flt(d.nppf) - flt(d.gis) - (flt(d.comm_all) / 2), 
+			"tds":d.tds if d.tds else 0, 
+			"health":d.health,
+			"receipt_number":d.receipt_number, 
+			"receipt_date":d.receipt_date,
+			"post_date":d.posting_date
+			}
 		data.append(row)
+	
 	#Leave Encashment 
 	if filters.employee:
-		# encash_data = frappe.db.sql("select a.encashment_date AS date, a.encashment_amount, a.encashment_tax, r.receipt_number, r.receipt_date from `tabLeave Encashment` a, `tabRRCO Receipt Entries` r where a.name = r.purchase_invoice and a.employee = %s and a.docstatus = 1 and a.encashment_date between \'" + filters.fiscal_year + "-01-01\' and \'" + filters.fiscal_year + "-12-31\'", filters.employee, as_dict=True) 
-		# encash_data = frappe.db.sql("select a.encashment_date, MONTH(a.encashment_date) AS month, YEAR(a.encashment_date) AS year, a.encashment_amount, a.encashment_tax, r.receipt_number, r.receipt_date from `tabLeave Encashment` a, `tabRRCO Receipt Entries` r where a.name = r.purchase_invoice and a.employee = %s and a.docstatus = 1 and a.encashment_date between \'" + filters.fiscal_year + "-01-01\' and \'" + filters.fiscal_year + "-12-31\'", filters.employee, as_dict=True) 
 		encash_data = frappe.db.sql("""SELECT 
 					DATE_FORMAT(a.encashment_date, '%d-%m-%Y') AS encashment_date,
 						r.receipt_number, 
@@ -66,97 +67,23 @@ def get_data( filters=None):
 						and a.encashment_date between '"""+ filters.fiscal_year + """-01-01' and '""" + filters.fiscal_year + """-12-31'""", as_dict=True) 
 		if encash_data:
 			for a in encash_data:
-				row = [
-				# str(a.date)[5:7], 
-				get_month(a.month)+"-"+str(a.year),
-				"Leave Encashment", 
-				0,
-				0, 
-				a.encashment_amount, 
-				0, 
-				0, 
-				0, 
-				a.encashment_amount, 
-				a.encashment_tax if a.encashment_tax else 0, 
-				0, 
-				a.receipt_number, 
-				a.receipt_date,
-				a.encashment_date]
-			data.append(row)
-
-		# night_shift = frappe.db.sql("""select 
-		# 					DATE_FORMAT(a.posting_date, '%d-%m-%Y') AS shift_date,
-		# 					b.rate, 
-		# 					b.amount,
-		# 					b.no_of_shifts, 
-		# 					b.shift_tax,
-		# 					MONTH(a.posting_date) AS month, 
-		# 					YEAR(a.posting_date) AS year	
-		# 				from `tabProcess Shift Payment` a, `tabShift Payment Details` b 
-		# 				where a.name = b.parent
-		# 				and a.docstatus = 1
-		# 				and a.posting_date between '"""+ filters.fiscal_year + """-01-01' and '""" + filters.fiscal_year + """-12-31'""", as_dict=True)
-		# if night_shift:
-		# 	for a in night_shift:
-		# 		row = [
-		# 		# str(a.date)[5:7], 
-		# 		get_month(a.month)+"-"+str(a.year),
-		# 		"Night Shift", 
-		# 		0,
-		# 		0, 
-		# 		a.amount, 
-		# 		0, 
-		# 		0, 
-		# 		0, 
-		# 		a.amount, 
-		# 		a.shift_tax if a.shift_tax else 0,
-		# 		0, 
-		# 		"", 
-		# 		"",
-		# 		a.shift_date]
-		# 	data.append(row)
-		# Overtime
-		# overtime_data = frappe.db.sql("""select 
-		# 			DATE_FORMAT(a.posting_date, '%d-%m-%Y') AS overtime_date,
-		# 			r.receipt_number, 
-		# 			MONTH(a.posting_date) AS month, YEAR(a.posting_date) AS year,
-		# 			a.total_amount as overtime_amt, a.overtime_tax, 
-		# 			r.receipt_number,
-		# 			DATE_FORMAT(r.receipt_date, '%d-%m-%Y') AS receipt_date
-		# 			from `tabOvertime Application` a, `tabRRCO Receipt Entries` r where a.name = r.purchase_invoice 
-		# 			and a.employee = """+filters.employee+""" and a.docstatus = 1 
-		# 			and a.posting_date between '"""	+ filters.fiscal_year + """-01-01' and '""" + filters.fiscal_year + """-12-31'""", as_dict=True)
-		# overtime_data = frappe.db.sql("""select 
-		# 			DATE_FORMAT(a.posting_date, '%d-%m-%Y') AS overtime_date,
-		# 			r.receipt_number, 
-		# 			MONTH(a.posting_date) AS month, YEAR(a.posting_date) AS year,
-		# 			a.total_amount as overtime_amt, a.overtime_tax, 
-		# 			r.receipt_number,
-		# 			DATE_FORMAT(r.receipt_date, '%d-%m-%Y') AS receipt_date
-		# 			from `tabOvertime Application` a LEFT JOIN `tabTDS Receipt Entry` r 
-		# 			ON a.name = r.invoice_no
-		# 			where a.employee = """+filters.employee+""" and a.docstatus = 1 
-		# 			and a.posting_date between '"""	+ filters.fiscal_year + """-01-01' and '""" + filters.fiscal_year + """-12-31'""", as_dict=True)
-		# if overtime_data:
-		# 	for a in overtime_data:
-		# 		row = [
-		# 		# str(a.date)[5:7], 
-		# 		get_month(a.month)+"-"+str(a.year),
-		# 		"Overtime", 
-		# 		0,
-		# 		0, 
-		# 		a.overtime_amt, 
-		# 		0, 
-		# 		0, 
-		# 		0, 
-		# 		a.overtime_amt, 
-		# 		a.overtime_tax if a.overtime_tax else 0,
-		# 		0, 
-		# 		a.receipt_number, 
-		# 		a.receipt_date,
-		# 		a.overtime_date]
-		# 	data.append(row)
-
+				row = {
+					"month-fyear":get_month(d.month)+"-"+d.fyear, 
+					"type":"Leave Encashment", 
+					"basic":0, 
+					"others":0, 
+					"total":a.encashment_amount, 
+					"pf":0,
+					"gis":0,
+					"totalPfGis":0, 
+					"taxable":flt(a.encashment_amount, 2), 
+					"tds":a.encashment_tax if a.encashment_tax else 0, 
+					"health":0,
+					"receipt_number":d.receipt_number, 
+					"receipt_date":d.receipt_date,
+					"post_date":d.posting_date
+					}
+				data.append(row)
 		#Bonus
 		bonus = frappe.db.sql("""
 					select b.name, b.fiscal_year AS fyear,
@@ -178,28 +105,23 @@ def get_data( filters=None):
 					where parent = %s and employee = %s
 				      """, (b.name, filters.employee), as_dict=1)
 			for a in amt:
-				row = [
-				get_month(b.month)+"-"+str(b.fyear),
-				"Bonus", 
-				0, 
-				0, 
-				a.amount, 
-				0, 
-				0, 
-				0, 
-				a.amount, 
-				a.tax_amount if a.tax_amount else 0,
-				0, 
-				b.receipt_number, 
-				b.receipt_date,
-				b.posting_date]	
-			data.append(row)
+				row =  {
+					"month-fyear":get_month(d.month)+"-"+d.fyear, 
+					"type":"Bonus", 
+					"basic":0, 
+					"others":0, 
+					"total":flt(a.amount,2), 
+					"pf":0,
+					"gis":0,
+					"totalPfGis":0, 
+					"taxable":flt(a.amount, 2), 
+					"tds":a.tax_amount if a.tax_amount else 0, 
+					"health":0,
+					"receipt_number":d.receipt_number, 
+					"receipt_date":d.receipt_date,
+					"post_date":d.posting_date}
+				data.append(row)
 		#PVBA
-		# pbva = frappe.db.sql("""
-		# 			select b.name, b.fiscal_year  as year, b.posting_date, MONTH(b.posting_date) as month, r.receipt_number, r.receipt_date 
-		# 			from tabPBVA b, `tabRRCO Receipt Entries` r
-		# 			where b.fiscal_year+1 = r.fiscal_year and b.docstatus = 1 and b.posting_date between %s and %s and r.purpose = 'PBVA' 
-		# 		      """, (str(filters.fiscal_year) + "-01-01", str(filters.fiscal_year) + "-12-31"), as_dict=1)
 		pbva = frappe.db.sql("""
 					select b.name, b.fiscal_year  as year, r.pbva,
 					DATE_FORMAT(b.posting_date, '%d-%m-%Y') AS posting_date,
@@ -220,22 +142,22 @@ def get_data( filters=None):
 					where parent = %s and employee = %s
 				      """, (b.name, filters.employee), as_dict=1)
 			for a in amt:
-				row = [get_month(b.month)+"-"+str(b.year), 
-					  "PBVA", 
-					  0, 
-					  0, 
-					  a.amount, 
-					  0, 
-					  0, 
-					  0, 
-					  a.amount, 
-					  a.tax_amount if a.tax_amount else 0, 
-					  0, 
-					  b.receipt_number, 
-					  b.receipt_date,
-					  b.posting_date]	
+				row =row =  {
+					"month-fyear":get_month(d.month)+"-"+d.fyear, 
+					"type":"PBVA", 
+					"basic":0, 
+					"others":0, 
+					"total":flt(a.amount,2), 
+					"pf":0,
+					"gis":0,
+					"totalPfGis":0, 
+					"taxable":flt(a.amount, 2), 
+					"tds":a.tax_amount if a.tax_amount else 0, 
+					"health":0,
+					"receipt_number":d.receipt_number, 
+					"receipt_date":d.receipt_date,
+					"post_date":d.posting_date}	
 				data.append(row)
-	# frappe.throw('{}'.format(data))
 	return data
 
 def validate_filters(filters):
@@ -328,7 +250,7 @@ def get_columns():
 		{
 		  "fieldname": "post_date",
 		  "label": "Posting Date",
-		  "fieldtype": "Data",
+		  "fieldtype": "Date",
 		  "width": 100
 		},
 	]
