0a23a800ea3a657df643766da149cee7b3333820
birendra-bit <bhujelbirendra3@gmail.com>
2023-04-04 13:34:55 +0600

    ltc process report changes made

--- hrms/hr/report/ltc_process_report/ltc_process_report.py
+++ hrms/hr/report/ltc_process_report/ltc_process_report.py
@@ -13,28 +13,36 @@ def get_data(filters):
 	if filters.uinput == "LTC":
 		query =  """
 			select t2.employee, t2.employee_name, (select tpn_number from tabEmployee e where e.name = t2.employee) as tpn, 
-			t2.branch, t2.bank_name, t2.bank_ac_no, t2.amount, 0, t2.amount
+			t2.branch,(select bank_name from tabEmployee e where e.name = t2.employee) as bank_name,
+			(select bank_ac_no from tabEmployee e where e.name = t2.employee) as bank_ac_no, 
+			t2.amount, 0, t2.amount
 			from `tabLeave Travel Concession` t1, `tabLTC Details` t2
 			where t2.parent = t1.name and t1.docstatus = 1"""
 
 	if filters.uinput == "PBVA":
 		query = """
 			select t2.employee, t2.employee_name, (select tpn_number from tabEmployee e where e.name = t2.employee) as tpn,
-			t2.branch, t2.bank_name, t2.bank_ac_no, t2.amount, t2.tax_amount, t2.balance_amount
+			t2.branch,(select bank_name from tabEmployee e where e.name = t2.employee) as bank_name,
+			(select bank_ac_no from tabEmployee e where e.name = t2.employee) as bank_ac_no, 
+			t2.amount, t2.tax_amount, t2.balance_amount
 			from `tabPBVA` t1, `tabPBVA Details` t2
 			where t2.parent = t1.name and t1.docstatus = 1"""
 
 	if filters.uinput == "Bonus":
 		query = """
 			select t2.employee, t2.employee_name, (select tpn_number from tabEmployee e where e.name = t2.employee) as tpn,
-			t2.branch, t2.bank_name, t2.bank_ac_no,  t2.amount, t2.tax_amount, t2.balance_amount 
+			t2.branch, (select bank_name from tabEmployee e where e.name = t2.employee) as bank_name,
+			(select bank_ac_no from tabEmployee e where e.name = t2.employee) as bank_ac_no, 
+			 t2.amount, t2.tax_amount, t2.balance_amount 
 			from `tabBonus` t1, `tabBonus Details` t2 
 			where t2.parent = t1.name and t1.docstatus = 1"""
 
 	if filters.uinput == "MPI":
 		query = """
 			select t2.employee, t2.employee_name, (select tpn_number from tabEmployee e where e.name = t2.employee) as tpn,
-			t2.branch,(select bank_name from tabEmployee e where e.name = t2.employee) as bank_name,(select bank_ac_no from tabEmployee e where e.name = t2.employee) as bank_ac_no,  t2.mpi_amount, t2.deduction_amount, t2.net_mpi_amount 
+			t2.branch,(select bank_name from tabEmployee e where e.name = t2.employee) as bank_name,
+			(select bank_ac_no from tabEmployee e where e.name = t2.employee) as bank_ac_no,  
+			t2.mpi_amount, t2.deduction_amount, t2.net_mpi_amount 
 			from `tabMPI Transaction` t1, `tabMPI Item` t2 
 			where t2.parent = t1.name and t1.docstatus = 1"""
 
