b3601b913eae7a58659667cf4f6eeead7de4a2ae
birendra-bit <bhujelbirendra3@gmail.com>
2023-04-14 16:09:24 +0600

    employee tds certificate made for employee

--- hrms/hr/report/employee_tds_certificate/employee_tds_certificate.py
+++ hrms/hr/report/employee_tds_certificate/employee_tds_certificate.py
@@ -15,151 +15,127 @@ def execute(filters=None):
 
 def get_data( filters=None):
 	data = []
-
-	salary = """select CONVERT(a.month, UNSIGNED) as month, a.gross_pay, a.fiscal_year as fyear,
-	(select b.amount from `tabSalary Detail` b where salary_component = 'Basic Pay' and b.parent = a.name) as basic_pay,
-	(select b.amount from `tabSalary Detail` b where salary_component = 'Salary Tax' and b.parent = a.name) as tds ,
-	(select b.amount from `tabSalary Detail` b where salary_component = 'PF' and b.parent = a.name) as nppf ,
-	ifnull((select b.amount from `tabSalary Detail` b where salary_component = 'GIS' and b.parent = a.name),0) as gis ,
-	(select b.amount from `tabSalary Detail` b where salary_component = 'Communication Allowance' and b.parent = a.name) as comm_all ,
-	(select b.amount from `tabSalary Detail` b where salary_component = 'Health Contribution' and b.parent = a.name) as health,
-	r.receipt_number, r.receipt_date, r.posting_date
-	from `tabSalary Slip` a, `tabTDS Receipt Entry` r
-	where a.fiscal_year = r.fiscal_year and a.docstatus = 1 and a.month = r.month
-	and r.purpose = 'Employee Salary' and a.fiscal_year = """ + str(filters.fiscal_year)
-
-	if filters.employee:
-		salary = salary + " AND a.employee = \'" + str(filters.employee) + "\'"
-
-	salary+=" order by r.receipt_date asc;"
-	datas = frappe.db.sql(salary, as_dict=True)
-	for d in datas:
-		row = {
-			"month-fyear":get_month(d.month)+"-"+d.fyear, 
+	# salary 
+	data += get_salary_data(filters)
+	#Leave Encashment 
+	data += get_leave_encashment(filters)
+	#Bonus
+	data += get_bonus(filters)
+	#PVBA
+	data += get_pbva(filters)
+	
+	return data
+def get_salary_data(filters):
+	data = []
+	for d in frappe.db.sql('''SELECT 
+								CONCAT(a.month,'-', a.fiscal_year) month_year, 
+								a.gross_pay, 
+								(SELECT b.amount FROM `tabSalary Detail` b WHERE b.parent = a.name AND b.salary_component = 'Basic Pay') AS basic_pay, 
+								(SELECT b.amount FROM `tabSalary Detail` b WHERE b.parent = a.name AND b.salary_component = 'Salary Tax') AS tds, 
+								(SELECT b.amount FROM `tabSalary Detail` b WHERE b.parent = a.name AND b.salary_component = 'PF') AS nppf, 
+								COALESCE((SELECT b.amount FROM `tabSalary Detail` b WHERE b.parent = a.name AND b.salary_component = 'GIS'), 0) AS gis, 
+								(SELECT b.amount FROM `tabSalary Detail` b WHERE b.parent = a.name AND b.salary_component = 'Communication Allowance') AS comm_all, 
+								(SELECT b.amount FROM `tabSalary Detail` b WHERE b.parent = a.name AND b.salary_component = 'Health Contribution') AS health, 
+								r.receipt_number, 
+								r.receipt_date, 
+								r.posting_date
+							FROM `tabSalary Slip` a
+							JOIN `tabTDS Receipt Entry` r ON a.fiscal_year = r.fiscal_year AND a.month = r.month
+							WHERE a.docstatus = 1 AND r.purpose = 'Employee Salary'
+							AND a.employee = '{employee}'
+							ORDER BY r.receipt_date ASC
+							'''.format(employee=filters.employee),as_dict=1):
+		data.append({
+			"month_year":d.month_year, 
 			"type":"Salary", 
-			"basic":d.basic_pay, 
-			"others":round(flt(d.gross_pay) - flt(d.basic_pay) - (flt(d.comm_all) / 2), 2), 
-			"total":round(flt(d.gross_pay)-(flt(d.comm_all) / 2),2), 
-			"pf":d.nppf,
-			"gis":d.gis,
-			"totalPfGis":flt(d.nppf)+flt(d.gis), 
+			"basic":flt(d.basic_pay,2), 
+			"others":flt(flt(d.gross_pay) - flt(d.basic_pay) - (flt(d.comm_all) / 2), 2), 
+			"total":flt(flt(d.gross_pay)-(flt(d.comm_all) / 2),2), 
+			"pf":flt(d.nppf,2),
+			"gis":flt(d.gis,2),
+			"totalPfGis":flt(flt(d.nppf)+flt(d.gis),2), 
 			"taxable":flt(d.gross_pay) - flt(d.nppf) - flt(d.gis) - (flt(d.comm_all) / 2), 
-			"tds":d.tds if d.tds else 0, 
-			"health":d.health,
+			"tds":flt(d.tds,2) if d.tds else 0, 
+			"health":flt(d.health,2),
 			"receipt_number":d.receipt_number, 
 			"receipt_date":d.receipt_date,
-			"post_date":d.posting_date
-			}
-		data.append(row)
-	
-	#Leave Encashment 
-	if filters.employee:
-		encash_data = frappe.db.sql("""SELECT 
-					DATE_FORMAT(a.encashment_date, '%d-%m-%Y') AS encashment_date,
-						r.receipt_number, 
-						MONTH(a.encashment_date) AS month, YEAR(a.encashment_date) AS year, 
-						a.encashment_amount, a.encashment_tax, 
-						r.receipt_number, 
-						DATE_FORMAT(r.receipt_date, '%d-%m-%Y') AS receipt_date
-					from `tabLeave Encashment` a, `tabTDS Receipt Entry` r
-					WHERE a.employee = """+filters.employee+""" and a.docstatus = 1 
-						and a.name = r.invoice_no
-						and a.encashment_date between '"""+ filters.fiscal_year + """-01-01' and '""" + filters.fiscal_year + """-12-31'""", as_dict=True) 
-		if encash_data:
-			for a in encash_data:
-				row = {
-					"month-fyear":get_month(d.month)+"-"+d.fyear, 
-					"type":"Leave Encashment", 
-					"basic":0, 
-					"others":0, 
-					"total":a.encashment_amount, 
-					"pf":0,
-					"gis":0,
-					"totalPfGis":0, 
-					"taxable":flt(a.encashment_amount, 2), 
-					"tds":a.encashment_tax if a.encashment_tax else 0, 
-					"health":0,
-					"receipt_number":d.receipt_number, 
-					"receipt_date":d.receipt_date,
-					"post_date":d.posting_date
-					}
-				data.append(row)
-		#Bonus
-		bonus = frappe.db.sql("""
-					select b.name, b.fiscal_year AS fyear,
-					r.receipt_number, DATE_FORMAT(b.posting_date, '%d-%m-%Y') AS posting_date,
-					MONTH(b.posting_date) AS month, 
-					r.receipt_number, 
-					DATE_FORMAT(r.receipt_date, '%d-%m-%Y') AS receipt_date
-					from tabBonus b, `tabTDS Receipt Entry` r
-					where b.fiscal_year = r.fiscal_year 
-					and b.docstatus = 1 
-					and b.posting_date between "{from_date}" and "{to_date}" 
-					and r.purpose = 'Bonus' 
-				""".format(from_date = str(filters.fiscal_year) + "-01-01",
-					  to_date = str(filters.fiscal_year) + "-12-31"), as_dict=1)
-		for b in bonus:
-			amt = frappe.db.sql("""
-				     	select amount, tax_amount, balance_amount  
-					from `tabBonus Details` 
-					where parent = %s and employee = %s
-				      """, (b.name, filters.employee), as_dict=1)
-			for a in amt:
-				row =  {
-					"month-fyear":get_month(d.month)+"-"+d.fyear, 
-					"type":"Bonus", 
-					"basic":0, 
-					"others":0, 
-					"total":flt(a.amount,2), 
-					"pf":0,
-					"gis":0,
-					"totalPfGis":0, 
-					"taxable":flt(a.amount, 2), 
-					"tds":a.tax_amount if a.tax_amount else 0, 
-					"health":0,
-					"receipt_number":d.receipt_number, 
-					"receipt_date":d.receipt_date,
-					"post_date":d.posting_date}
-				data.append(row)
-		#PVBA
-		pbva = frappe.db.sql("""
-					select b.name, b.fiscal_year  as year, r.pbva,
-					DATE_FORMAT(b.posting_date, '%d-%m-%Y') AS posting_date,
-					MONTH(b.posting_date) as month, 
-					r.receipt_number, 
-					DATE_FORMAT(r.receipt_date, '%d-%m-%Y') AS receipt_date
-					from tabPBVA b, `tabTDS Receipt Entry` r
-					where b.fiscal_year = r.fiscal_year 
-					and b.docstatus = 1 
-					and b.posting_date between "{fdate}" and "{tdate}" 
-					and r.purpose = 'PBVA' 
-					and b.name = r.pbva
-				      """.format(fdate= str(filters.fiscal_year) + "-01-01", tdate = str(filters.fiscal_year) + "-12-31"), as_dict=1)
-		for b in pbva:
-			amt = frappe.db.sql("""
-				     	select amount, tax_amount, balance_amount  
-					from `tabPBVA Details` 
-					where parent = %s and employee = %s
-				      """, (b.name, filters.employee), as_dict=1)
-			for a in amt:
-				row =row =  {
-					"month-fyear":get_month(d.month)+"-"+d.fyear, 
-					"type":"PBVA", 
-					"basic":0, 
-					"others":0, 
-					"total":flt(a.amount,2), 
-					"pf":0,
-					"gis":0,
-					"totalPfGis":0, 
-					"taxable":flt(a.amount, 2), 
-					"tds":a.tax_amount if a.tax_amount else 0, 
-					"health":0,
-					"receipt_number":d.receipt_number, 
-					"receipt_date":d.receipt_date,
-					"post_date":d.posting_date}	
-				data.append(row)
+			"posting_date":d.posting_date
+			})
 	return data
-
+def get_leave_encashment(filters):
+	return frappe.db.sql("""SELECT 
+								a.encashment_date AS posting_date,
+								r.receipt_number,
+								'Leave Encashment' as type,
+								CONCAT(MONTH(a.encashment_date),'-', YEAR(a.encashment_date)) AS month_year,
+								ROUND(a.encashment_amount, 2) AS total,
+								ROUND(a.encashment_amount, 2) AS taxable,
+								ROUND(a.encashment_tax, 2) AS tds,
+								r.receipt_number,
+								r.receipt_date,
+								0 AS basic,
+								0 AS other,
+								0 AS pf,
+								0 AS gis,
+								0 AS totalPfGis,
+								0 AS others,
+								0 AS health
+								FROM `tabLeave Encashment` a
+								JOIN `tabTDS Receipt Entry` r ON a.name = r.invoice_no
+								WHERE a.employee = '{employee}'
+								AND a.docstatus = 1
+								AND a.encashment_date BETWEEN '{from_date}' AND '{to_date}'
+						""".format(employee=filters.employee,from_date = getdate(str(filters.fiscal_year) + "-01-01"),
+					  to_date = getdate(str(filters.fiscal_year) + "-12-31")), as_dict=True) 
+def get_bonus(filters):
+	return frappe.db.sql("""
+					SELECT CONCAT(MONTH(b.posting_date), '-', b.fiscal_year) AS month_year,
+						r.receipt_number,
+						b.posting_date,
+						r.receipt_number,
+						r.receipt_date,
+						'Bonus' AS type,
+						0 AS basic,
+						0 AS others,
+						ROUND(bd.amount,2) AS total,
+						0 AS pf,
+						0 AS gis,
+						0 AS totalPfGis,
+						ROUND(bd.amount,2) AS taxable,
+						ROUND(ifnull(bd.tax_amount,0),2) as tds,
+						0 AS health
+					FROM tabBonus b
+					JOIN `tabTDS Receipt Entry` r ON b.fiscal_year = r.fiscal_year
+					JOIN `tabBonus Details` bd ON b.name = bd.parent
+					WHERE b.docstatus = 1
+					AND b.posting_date BETWEEN '{from_date}' AND '{to_date}'
+					AND r.purpose = 'Bonus'
+					AND bd.employee = '{employee}'
+				""".format(from_date = getdate(str(filters.fiscal_year) + "-01-01"),
+					  to_date = getdate(str(filters.fiscal_year) + "-12-31"), employee= filters.employee), as_dict=1)
+def get_pbva(filters):
+	return frappe.db.sql("""SELECT 
+									ROUND(bd.amount,2) AS total, 
+									ROUND(bd.amount,2) AS taxable, 
+									ROUND(ifnull(bd.tax_amount,0),2) as tds,
+									CONCAT(MONTH(b.posting_date),'-',
+									b.fiscal_year) AS month_year,
+									r.receipt_number,
+									'PBVA' AS type, 
+									0 as basic, 
+									0 as others, 
+									0 AS pf, 
+									0 AS gis, 
+									0 AS totalPfGis, 
+									0 AS health,
+									r.receipt_date,	
+									b.posting_date
+								FROM tabPBVA b
+								INNER JOIN `tabTDS Receipt Entry` r ON b.fiscal_year = r.fiscal_year AND r.purpose = 'PBVA'
+								LEFT JOIN `tabPBVA Details` bd ON b.name = bd.parent AND bd.employee = '{employee}'
+								WHERE b.docstatus = 1 AND b.posting_date BETWEEN "{fdate}" AND "{tdate}"
+				      """.format(fdate= getdate(str(filters.fiscal_year) + "-01-01"), tdate = getdate(str(filters.fiscal_year) + "-12-31"), employee = filters.employee), as_dict=1)
+	
 def validate_filters(filters):
 	if not filters.fiscal_year:
 		frappe.throw(_("Fiscal Year {0} is required").format(filters.fiscal_year))
@@ -170,7 +146,7 @@ def validate_filters(filters):
 def get_columns():
 	return [
 		{
-		  "fieldname": "month-fyear",
+		  "fieldname": "month_year",
 		  "label": "Month-Year",
 		  "fieldtype": "Data",
 		  "width": 100
@@ -248,10 +224,10 @@ def get_columns():
 		  "width": 130
 		},
 		{
-		  "fieldname": "post_date",
+		  "fieldname": "posting_date",
 		  "label": "Posting Date",
 		  "fieldtype": "Date",
-		  "width": 100
+		  "width": 130
 		},
 	]
 
