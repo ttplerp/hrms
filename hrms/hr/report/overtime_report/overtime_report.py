14f05aaa45e75f1c72c870533c05e5b977ea3cb1
birendra-bit <bhujelbirendra3@gmail.com>
2023-04-24 11:30:11 +0600

    filter fixed in OT report and standard filter set in ot application

--- hrms/hr/report/overtime_report/overtime_report.py
+++ hrms/hr/report/overtime_report/overtime_report.py
@@ -18,24 +18,24 @@ def execute(filters=None):
 
 def get_columns(data):
 	columns = [
-		_("Employee ID") + ":Link/Overtime Application:150",
+		_("Employee ID") + ":Link/Overtime Application:130",
 		_("Employee Name") + "::150",
 		_("Branch") + ":Link/Branch:150",
 		_("Designation") + ":Link/Designation:120",
-		_("Grade") + ":Link/Employee Grade:120",
+		_("Grade") + ":Link/Employee Grade:100",
 		_("From Date/Time") + "::150",
 		_("To Date/Time") + "::150",
-		_("Number of Hours") + "::100",
+		_("No. of Hours") + "::130",
 		_("Amount") + ":Currency:120",
 	]
 	return columns
 
 def get_data(filters):
-	conditions, filters = get_conditions(filters)
+	conditions = get_conditions(filters)
 	data = frappe.db.sql("""
 		SELECT 
 			oa.employee, oa.employee_name, oa.branch, oa.designation, oa.grade, 
-			oai.from_date, oai.to_date, oai.number_of_hours, oa.total_amount
+			oai.from_date, oai.to_date, ROUND(oai.number_of_hours,2), ROUND(oa.total_amount,2)
 		FROM `tabOvertime Application` oa, `tabOvertime Application Item` oai
 		WHERE oa.name = oai.parent
 		AND oai.to_date >= '{from_date}'
@@ -45,6 +45,8 @@ def get_data(filters):
 
 def get_conditions(filters):
 	conditions = ""
-	if filters.branch:
-		conditions += """and branch="{}" """.format(filters.branch)
-	return conditions, filters
+	if filters.get("branch"):
+		conditions += "and oa.branch='{}' ".format(filters.get("branch"))
+	if filters.get("employee"):
+		conditions += " and oa.employee = '{}' ".format(filters.get("employee"))
+	return conditions
