f639d73da30ffa96121bddb931e0f113225b4638
RinzinNorbu <rinzinn1996@gmail.com>
2023-05-08 10:54:45 +0600

    employee seperation changes

--- hrms/hr/doctype/employee_separation/employee_separation.js
+++ hrms/hr/doctype/employee_separation/employee_separation.js
@@ -9,21 +9,19 @@ frappe.ui.form.on('Employee Separation', {
 		frm.add_fetch("employee_separation_template", "employee_grade", "employee_grade");
 	},
 
+	onload_post_render: function(frm){
+		// console.log(cur_frm.doc.sb_exit_interview)
+		// if (frm.doc.docstatus === 1 && frappe.user.has_role(["HR User"])){
+		// 	cur_frm.set_df_property("sb_exit_interview","hidden",0);
+		// }
+		// else if(frm.doc.docstatus === 1){
+		// 	cur_frm.set_df_property("sb_exit_interview","hidden",1);
+		// }
+		// frm.refresh_fields()
+
+	},
 	refresh: function(frm) {
-		if (frm.doc.employee) {
-			frm.add_custom_button(__('Employee'), function() {
-				frappe.set_route("Form", "Employee", frm.doc.employee);
-			},__("View"));
-		}
-		if (frm.doc.project) {
-			frm.add_custom_button(__('Project'), function() {
-				frappe.set_route("Form", "Project", frm.doc.project);
-			},__("View"));
-			frm.add_custom_button(__('Task'), function() {
-				frappe.set_route('List', 'Task', {project: frm.doc.project});
-			},__("View"));
-		}
-		if(cur_frm.doc.docstatus == 1 && cur_frm.doc.employee_benefit_claim_status == "Not Claimed"){
+		if(cur_frm.doc.docstatus == 1 && cur_frm.doc.employee_benefits_status == "Not Claimed" && cur_frm.doc.clearance_acquired == 1){
 			frm.add_custom_button("Create Employee Benefit", function(){
 				frappe.model.open_mapped_doc({
 					method: "hrms.hr.doctype.employee_separation.employee_separation.make_employee_benefit",
@@ -31,8 +29,48 @@ frappe.ui.form.on('Employee Separation', {
 				})
 			});
 		}
+		if(cur_frm.doc.docstatus == 1 && cur_frm.doc.employee_benefits_status == "Not Claimed" && cur_frm.doc.clearance_acquired == 0){
+			frm.add_custom_button("Create Employee Clearance", function(){
+				frappe.model.open_mapped_doc({
+					method: "hrms.hr.doctype.employee_separation.employee_separation.make_separation_clearance",
+					frm: me.frm
+				})
+			});
+		}
+		// if (frm.doc.project) {
+		// 	frm.add_custom_button(__('Project'), function() {
+		// 		frappe.set_route("Form", "Project", frm.doc.project);
+		// 	},__("View"));
+		// 	frm.add_custom_button(__('Task'), function() {
+		// 		frappe.set_route('List', 'Task', {project: frm.doc.project});
+		// 	},__("View"));
+		// }
+		// if (frm.doc.docstatus === 1 && frm.doc.project) {
+		// 	frappe.call({
+		// 		method: "erpnext.hr.utils.get_boarding_status",
+		// 		args: {
+		// 			"project": frm.doc.project
+		// 		},
+		// 		callback: function(r) {
+		// 			if (r.message) {
+		// 				frm.set_value('boarding_status', r.message);
+		// 			}
+		// 			refresh_field("boarding_status");
+		// 		}
+		// 	});
+		// }
 	},
+	// superannuation: function(frm){
+	// 	if(frm.doc.superannuation==1){
+	// 		frm.set_df_property("q24","reqd",1);
+	// 	}
+	// 	else
+	// 	{
+	// 		frm.set_df_property("q24","reqd",0);
+	// 	}
+	// 	frm.refresh_fields();
 
+	// },
 	employee_separation_template: function(frm) {
 		frm.set_value("activities" ,"");
 		if (frm.doc.employee_separation_template) {
@@ -53,5 +91,23 @@ frappe.ui.form.on('Employee Separation', {
 				}
 			});
 		}
+	},
+
+	// employee: function(frm) {
+	// 	if (frm.doc.employee) {				    
+	// 		frappe.call({			
+	// 		   method: 'get_training_obligation_list',
+	// 		   doc: frm.doc,
+	// 		   callback: (r)=> {
+	// 			frm.refresh_field('obligation_history');
+	// 			   frm.refresh_fields()				
+	// 		   }
+	// 	   })
+	//    }	
+	// },
+
+	reason_for_resignation: function(frm){
+		var fields = ["Superannuation", "Demise"];
+		frm.toggle_reqd("q24", !fields.includes(frm.doc.reason_for_resignation));
 	}
 });
