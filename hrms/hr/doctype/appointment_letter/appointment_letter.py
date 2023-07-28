9e41133da6995127e8e76e23f7997773ac0e49ea
Rinzin <rinzinn1996@gmail.com>
2023-07-28 13:22:41 +0600

    hrms changes

--- hrms/hr/doctype/appointment_letter/appointment_letter.py
+++ hrms/hr/doctype/appointment_letter/appointment_letter.py
@@ -10,20 +10,20 @@ class AppointmentLetter(Document):
 	pass
 
 
-@frappe.whitelist()
-def get_appointment_letter_details(template):
-	body = []
-	intro = frappe.get_list(
-		"Appointment Letter Template",
-		fields=["introduction", "closing_notes"],
-		filters={"name": template},
-	)[0]
-	content = frappe.get_all(
-		"Appointment Letter content",
-		fields=["title", "description"],
-		filters={"parent": template},
-		order_by="idx",
-	)
-	body.append(intro)
-	body.append({"description": content})
-	return body
+# @frappe.whitelist()
+# def get_appointment_letter_details(template):
+# 	body = []
+# 	intro = frappe.get_list(
+# 		"Appointment Letter Template",
+# 		fields=["introduction", "closing_notes"],
+# 		filters={"name": template},
+# 	)[0]
+# 	content = frappe.get_all(
+# 		"Appointment Letter content",
+# 		fields=["title", "description"],
+# 		filters={"parent": template},
+# 		order_by="idx",
+# 	)
+# 	body.append(intro)
+# 	body.append({"description": content})
+# 	return body
