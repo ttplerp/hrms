6e1bf794a59a50defb2a70f7e86ccdfb0d8e16d0
kinleydorji <kd77144@gmail.com>
2023-08-03 14:58:15 +0600

    employee transfer request and employee transfer

--- hrms/hr/doctype/employee_transfer_request/employee_transfer_request.js
+++ hrms/hr/doctype/employee_transfer_request/employee_transfer_request.js
@@ -2,13 +2,54 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Employee Transfer Request', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+		frm.set_query("employee",function(doc) {
+			return {
+				query: "erpnext.controllers.queries.filter_division_employees",
+				filters: {
+					'user': frappe.session.user
+				}
+			}
+		});
+		if (frm.doc.docstatus == 1) {
+			frappe.call({
+				method:"check_employee_transfer",
+				doc: frm.doc,
+				callback: function(r){
+					if(r.message==0){
+						frm.add_custom_button(__("Employee Transfer"),
+						() => frm.events.make_employee_transfer(frm), __("Make"));
+					}
+				}
+			})
 
-	// },
+		}
+	},
+	make_employee_transfer: function (frm) {
+		frappe.model.open_mapped_doc({
+			method: "hrms.hr.doctype.employee_transfer_request.employee_transfer_request.make_employee_transfer",
+			frm: frm,
+			run_link_triggers: true
+		});
+	},
+
 	employee: function(frm){
 		frappe.call({
 			method: "validate_requested_by",
 			doc: frm.doc
+		})
+		frappe.call({
+			method: "get_employee_details",
+			doc: frm.doc,
+			callback: function(r){
+				frm.set_value("designation", r.message[0])
+				frm.set_value("branch", r.message[1])
+				frm.set_value("employee_name", r.message[2])
+				frm.set_value("division", r.message[3])
+				frm.set_value("cell_number", r.message[4])
+				frm.set_value("email", r.message[5])
+				frm.refresh_fields();
+			}
 		})
 	}
 });
--- hrms/hr/doctype/employee_transfer_request/employee_transfer_request.py
+++ hrms/hr/doctype/employee_transfer_request/employee_transfer_request.py
@@ -4,6 +4,7 @@
 import frappe
 from frappe.model.document import Document
 from erpnext.custom_workflow import validate_workflow_states, notify_workflow_states
+from frappe.model.mapper import get_mapped_doc
 
 class EmployeeTransferRequest(Document):
 	def validate(self):
@@ -13,12 +14,60 @@ class EmployeeTransferRequest(Document):
 			notify_workflow_states(self)
 
 	@frappe.whitelist()
+	def get_employee_details(self):
+		if self.employee:
+			designation =  frappe.db.get_value("Employee", self.employee, "designation")
+			branch =  frappe.db.get_value("Employee", self.employee, "branch")
+			employee_name = frappe.db.get_value("Employee", self.employee, "employee_name")
+			division = frappe.db.get_value("Employee", self.employee, "division")
+			cell_number = frappe.db.get_value("Employee", self.employee, "cell_number")
+			email = frappe.db.get_value("Employee", self.employee, "personal_email")
+
+			return designation, branch, employee_name, division, cell_number, email
+
+	@frappe.whitelist()
+	def check_employee_transfer(self):
+		created = 0
+		doc = frappe.db.sql("""
+			select name from `tabEmployee Transfer` where employee_transfer_request_id = '{}'
+			and docstatus > 2
+		""".format(str(self.name)))
+		if len(doc) > 0:
+			created = 1
+		return created
+
+	@frappe.whitelist()
 	def validate_requested_by(self):
 		if self.get("__islocal"):
 			is_dept_head = 0
-			current_logged_in_emp, department = frappe.db.get_value("Employee", {"user_id":frappe.session.user}, ["name","department"])
-			dept_head = frappe.db.get_value("Department", department, "approver")
-			if not dept_head:
-				frappe.throw("Department Approver not set for department {} in Department Tree".format(department))
-			if current_logged_in_emp != dept_head:
-				frappe.throw("Only Department Head {} of department {} can request for Employee Transfer".format(dept_head, department))
+			current_logged_in_emp, division = frappe.db.get_value("Employee", {"user_id":frappe.session.user}, ["name","division"])
+			div_head = frappe.db.get_value("Department", division, "approver")
+			if not div_head:
+				frappe.throw("Division Approver not set for division {} in Department Tree".format(division))
+			if current_logged_in_emp != div_head:
+				frappe.throw("Only Division Head {} of department {} can request for Employee Transfer".format(div_head, division))
+
+@frappe.whitelist()
+def make_employee_transfer(source_name, target_doc=None):
+	def update_master(source_doc, target_doc, source_partent):
+		#target_doc.project = source_doc.project
+		# target_doc.invoice_title = str(target_doc.project) + "(Project Invoice)"
+		# target_doc.reference_doctype = "MB Entry"
+		# target_doc.reference_name	= source_doc.name
+		pass
+
+	def update_reference(source_doc, target_doc, source_parent):
+			pass
+			
+	doclist = get_mapped_doc("Employee Transfer Request", source_name, {
+		"Employee Transfer Request": {
+						"doctype": "Employee Transfer",
+						# "field_map":{
+						# 		"project": "project",
+						# 		"branch": "branch",
+						# 		"customer": "customer"
+						# },
+						"postprocess": update_master
+				},
+	}, target_doc)
+	return doclist		
