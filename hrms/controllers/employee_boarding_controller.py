32138881d629d04c1b24fcb56edc2ee1f4ba68d9
Dendup67 <dendup1998@gmail.com>
2023-08-11 16:44:40 +0600

    Employee onboarding removed

--- hrms/controllers/employee_boarding_controller.py
+++ hrms/controllers/employee_boarding_controller.py
@@ -24,30 +24,31 @@ class EmployeeBoardingController(Document):
 				activity.task = ""
 
 	def on_submit(self):
+		pass
 		# create the project for the given employee onboarding
-		project_name = _(self.doctype) + " : "
-		if self.doctype == "Employee Onboarding":
-			project_name += self.job_applicant
-		else:
-			project_name += self.employee
+		# project_name = _(self.doctype) + " : "
+		# if self.doctype == "Employee Onboarding":
+		# 	project_name += self.job_applicant
+		# else:
+		# 	project_name += self.employee
 
-		project = frappe.get_doc(
-			{
-				"doctype": "Project",
-				"project_name": project_name,
-				"expected_start_date": self.date_of_joining
-				if self.doctype == "Employee Onboarding"
-				else self.resignation_letter_date,
-				'status': "Open",
-				"department": self.department,
-				"company": self.company,
-			}
-		).insert(ignore_permissions=True, ignore_mandatory=True)
+		# project = frappe.get_doc(
+		# 	{
+		# 		"doctype": "Project",
+		# 		"project_name": project_name,
+		# 		"expected_start_date": self.date_of_joining
+		# 		if self.doctype == "Employee Onboarding"
+		# 		else self.resignation_letter_date,
+		# 		'status': "Open",
+		# 		"department": self.department,
+		# 		"company": self.company,
+		# 	}
+		# ).insert(ignore_permissions=True, ignore_mandatory=True)
 
-		self.db_set("project", project.name)
-		self.db_set("boarding_status", "Pending")
-		self.reload()
-		self.create_task_and_notify_user()
+		# self.db_set("project", project.name)
+		# self.db_set("boarding_status", "Pending")
+		# self.reload()
+		# self.create_task_and_notify_user()
 
 	def create_task_and_notify_user(self):
 		# create the task for the given project and assign to the concerned person
