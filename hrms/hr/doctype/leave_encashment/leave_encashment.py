923be3a11434efab7a224739c7197ad6200c1c36
Rinzin <rinzinn1996@gmail.com>
2023-09-19 16:20:01 +0600

    leave encashment changes

--- hrms/hr/doctype/leave_encashment/leave_encashment.py
+++ hrms/hr/doctype/leave_encashment/leave_encashment.py
@@ -157,13 +157,13 @@ class LeaveEncashment(Document):
 			)
 		)
 		employee_group = frappe.db.get_value("Employee", self.employee, "employee_group")
-		encashable_days = frappe.db.get_value("Employee Group", employee_group, "max_encashment_days")
+		encashable_days = frappe.db.get_value("Employee Group", employee_group, "encashment_min")
 
-		if self.leave_balance < frappe.db.get_value("Employee Group", employee_group, "max_encashment_days"):
+		if self.leave_balance < cint(encashable_days):
 			frappe.msgprint(_("Minimum '{}' days is Mandatory for Encashment").format(cint(encashable_days)),title="Leave Balance")
 		
 		self.encashable_days = encashable_days if encashable_days > 0 else 0
-		self.encashment_days = encashable_days
+		self.encashment_days = frappe.db.get_value("Employee Group", employee_group, "max_encashment_days")
 		# per_day_encashment = frappe.db.get_value("Salary Structure", salary_structure, "leave_encashment_amount_per_day")
 		
 		# getting encashment amount from salary structure
