e9c0cbcda1998513da263427b1e10736acaa8de3
Nyuethyue <dawanyuethyuetshering111@gmail.com>
2023-10-16 11:36:57 +0600

    get pe for current month

--- hrms/pms/doctype/performance_evaluation/performance_evaluation.py
+++ hrms/pms/doctype/performance_evaluation/performance_evaluation.py
@@ -71,7 +71,7 @@ def get_permission_query_conditions(user):
 	if user == "Administrator":
 		return
 	if "HR Manager" in user_roles:
-		return """CAST(`tabPerformance Evaluation`.month as signed) = Month('{}')""".format(today())
+		return """(CAST(`tabPerformance Evaluation`.month as signed) = Month('{}') and `tabPerformance Evaluation`.docstatus = 0)""".format(today())
 
 	return """(
 		`tabPerformance Evaluation`.owner = '{user}'
