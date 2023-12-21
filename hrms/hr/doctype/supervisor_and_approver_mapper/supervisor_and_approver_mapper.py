d60aed9ec81cf41689b23577216ed8a0e56aa993
Nyuethyue <dawanyuethyuetshering111@gmail.com>
2023-12-21 13:00:27 +0600

    supervisor mapper

--- hrms/hr/doctype/supervisor_and_approver_mapper/supervisor_and_approver_mapper.py
+++ hrms/hr/doctype/supervisor_and_approver_mapper/supervisor_and_approver_mapper.py
@@ -1,8 +1,17 @@
 # Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class SupervisorAndApproverMapper(Document):
-	pass
+	def validate(self):
+		self.check_duplicate_branch()
+	
+	def check_duplicate_branch(self):
+		unique_items = set()
+		for a in self.supervisor_items:
+			if a.branch in unique_items:
+				frappe.throw("Duplicate Branch <strong>{}</strong> found in supervisor list table.".format(a.branch))
+			else:
+				unique_items.add(a.branch)
