2189ea5a62217251e70aeaabe2d32deb1951ef13
thukten <thukday@gmail.com>
2024-05-20 12:17:15 +0600

    OT Update Tool

--- hrms/hr/doctype/ot_update_tools/ot_update_tools.js
+++ hrms/hr/doctype/ot_update_tools/ot_update_tools.js
@@ -12,15 +12,29 @@ frappe.ui.form.on('OT Update Tools', {
 				]
 			}
 		}
+		frm.fields_dict['temporary_staff_details'].grid.get_field('name_code').get_query = function(doc, cdt, cdn) {
+			var child = locals[cdt][cdn];
+			return {    
+				filters:[
+					['deployed_branch', '=', frm.doc.branch]
+				]
+			}
+		}
 	},
 	memo: function(frm){
 		$.each(frm.doc.ot_details || [], function(i, v) {
+			frappe.model.set_value(v.doctype, v.name, "remarks", frm.doc.memo);
+		});
+		$.each(frm.doc.temporary_staff_details || [], function(i, v) {
 			frappe.model.set_value(v.doctype, v.name, "remarks", frm.doc.memo);
 		});
 	},
 	overtime_type: function(frm) {
 		$.each(frm.doc.ot_details || [], function(i, v) {
 			frappe.model.set_value(v.doctype, v.name, "overtime_type", frm.doc.overtime_type);
+		});
+		$.each(frm.doc.temporary_staff_details || [], function(i, v) {
+			frappe.model.set_value(v.doctype, v.name, "ot_type", frm.doc.overtime_type);
 		});
 	}
 })
@@ -38,6 +52,7 @@ frappe.ui.form.on('OT tools items', {
 	"employee": function(frm, cdt,cdn) {
 		frappe.model.set_value(cdt, cdn, "remarks", frm.doc.memo);
 		frappe.model.set_value(cdt, cdn, "overtime_type", frm.doc.overtime_type);
+		get_rate_base_on_overtime_type(frm, cdt, cdn);
 		calculate_time(frm, cdt, cdn);
 	},
 	"from_date": function(frm, cdt, cdn) {
@@ -78,6 +93,7 @@ function get_rate_base_on_overtime_type(frm, cdt, cdn){
 function calculate_time(frm, cdt, cdn) {
 	var item = locals[cdt][cdn];
 	var total_amount = 0;
+	var temp_total_amount = 0;
 	// var total_time = 0;
 	if(item.overtime_type == "Sunday Overtime (Half Day)"){
 		frappe.model.set_value(cdt, cdn, "number_of_hours", 4);
@@ -103,8 +119,97 @@ function calculate_time(frm, cdt, cdn) {
 			total_amount += item.ot_amount
 		}	
 	})
-	frm.set_value("total_amount", total_amount)
-	cur_frm.refresh_field("total_amount")
+	frm.doc.temporary_staff_details.forEach(function(item) {
+		if(item.temp_ot_amount) {
+			temp_total_amount += item.temp_ot_amount
+		}	
+	})
+	frm.set_value("total_amount", total_amount+temp_total_amount);
+	cur_frm.refresh_field("total_amount");
 }
 
+frappe.ui.form.on('Temporary staff items', {
+	"refresh": function(frm,cdt,cdn) {
+		frm.set_query("name_code", function() {
+			return {
+				filters:[
+					['deployed_branch', '=', frm.doc.branch]
+					]
+			};
+		});	
+	},
+	"name_code": function(frm, cdt,cdn) {
+		frappe.model.set_value(cdt, cdn, "remarks", frm.doc.memo);
+		frappe.model.set_value(cdt, cdn, "ot_type", frm.doc.overtime_type);
+		get_temp_overtime_rate(frm, cdt, cdn);
+		calculate_ot(frm, cdt, cdn);
+	},
+	"ot_hours": function(frm, cdt, cdn) {
+		calculate_ot(frm,cdt, cdn);
+	},
+	"approved_ot_hours": function(frm, cdt, cdn){
+		calculate_ot(frm, cdt, cdn);
+	},
+	"ot_type": function(frm, cdt, cdn){
+		get_temp_overtime_rate(frm, cdt, cdn);
+		calculate_ot(frm, cdt, cdn);
+	}
+})
+
+function get_temp_overtime_rate(frm, cdt, cdn){
+	var i = locals[cdt][cdn]
+	frappe.call({
+		method: "hrms.hr.doctype.ot_update_tools.ot_update_tools.get_temp_overtime_rate",
+		args: {
+				staff: i.name_code
+			},
+		callback: function(r){
+			console.log(r.message)
+			frappe.model.set_value(cdt, cdn, "rates", r.message[0]);
+			frappe.model.set_value(cdt, cdn, "half_day", r.message[1]);
+			frappe.model.set_value(cdt, cdn, "full_day", r.message[2]);
+			frm.refresh_fields();
+		}
+	})	
+}
+
+function calculate_ot(frm, cdt, cdn){
+	var item = locals[cdt][cdn];
+	var total_amount = 0;
+	var temp_total_amount = 0;
+	// var total_time = 0;
+	if(item.overtime_type == "Sunday Overtime (Half Day)"){
+		frappe.model.set_value(cdt, cdn, "ot_hours", 4);
+		frappe.model.set_value(cdt, cdn, "approved_ot_hours", 4);
+		frappe.model.set_value(cdt, cdn, "temp_ot_amount", flt(item.half_day))
+	}
+	else if(item.overtime_type == "Sunday Overtime (Full Day)"){
+		frappe.model.set_value(cdt, cdn, "ot_hours", 8);
+		frappe.model.set_value(cdt, cdn, "approved_ot_hours", 8);
+		frappe.model.set_value(cdt, cdn, "temp_ot_amount", flt(item.full_day))
+	}else{
+		/*
+		const from_date = new Date('2024-05-11'+' '+ item.from_date);
+		const to_date = new Date('2024-05-11'+' '+ item.to_date);
+		var hours = moment(to_date).diff(moment(from_date), "seconds") / 3600;
+		frappe.model.set_value(cdt, cdn, "approved_ot_hours", hours);
+		*/
+		if(item.approved_ot_hours){
+			frappe.model.set_value(cdt, cdn, "temp_ot_amount", flt(item.rates * item.approved_ot_hours));
+		}
+	}
+	
+	frm.doc.temporary_staff_details.forEach(function(item) {
+		if(item.temp_ot_amount) {
+			temp_total_amount += item.temp_ot_amount
+		}	
+	})
+	frm.doc.ot_details.forEach(function(item) {
+		if(item.ot_amount) {
+			total_amount += item.ot_amount
+		}	
+	})
+	frm.set_value("total_amount", total_amount+temp_total_amount);
+	cur_frm.refresh_field("total_amount");
+}
 
--- hrms/hr/doctype/ot_update_tools/ot_update_tools.py
+++ hrms/hr/doctype/ot_update_tools/ot_update_tools.py
@@ -23,6 +23,8 @@ class OTUpdateTools(Document):
 			for b in self.get("ot_details"):
 				if a.employee == b.employee:
 					duplicate += 1
+			if duplicate > 1:
+				frappe.throw("Duplicate entry for Employee <b>{}, {}</b>. Please check".format(a.employee, a.employee_name))
 
 			if self.name:
 				for c in frappe.db.sql("""
@@ -35,32 +37,73 @@ class OTUpdateTools(Document):
 								and t.name != '{}'
 							""".format(a.employee, self.posting_date, self.name), as_dict=True):
 					frappe.throw("Employee <b>{}, {}</b> OT for date <b>{}</b> is already recorded with <b>{} </b>".format(a.employee, a.employee_name, self.posting_date,c.name))
+		
+
+		#Check for Temporary Staffs
+		for a in self.get("temporary_staff_details"):
+			duplicate = 0
+			doc = frappe.get_doc("TES_Temporary Staffs", a.name_code)
+			for b in self.get("temporary_staff_details"):
+				if a.name_code == b.name_code:
+					duplicate += 1
 			
 			if duplicate > 1:
-				frappe.throw("Duplicate entry for Employee <b>{}, {}</b>. Please check".format(a.employee, a.employee_name)) 
+				frappe.throw("Duplicate entry for Temporary Employee <b>{}, {}</b>. Please check".format(a.name_code, a.full_name)) 
 
+			if self.name:
+				for c in frappe.db.sql("""
+								select t.name
+								from `tabOT Update Tools` t
+								inner join `tabTemporary staff items` i on t.name=i.parent
+								where i.name_code ='{}'
+								and t.posting_date='{}'
+								and t.docstatus!=2
+								and t.name != '{}'
+							""".format(a.name_code, self.posting_date, self.name), as_dict=True):
+					frappe.throw("Temporary Employee <b>{}, {}</b> OT for date <b>{}</b> is already recorded with <b>{} </b>".format(a.name_code, a.full_name, self.posting_date,c.name))
+ 
 	def calculate_total_amount(self):
-		total_amount = 0.00
+		total_amount, temp_total_amount = 0.00, 0.00
 		for row in self.ot_details:
 			if not row.ot_amount:
-				frappe.throw("OT amount cannot be zero")
+				frappe.throw("OT amount cannot be zero for {}, {}".format(row.employee, row.employee_name))
 			if not row.approved_ot_hrs:
-				frappe.throw("Please enter approved OT hours") 
+				frappe.throw("Please enter approved OT hours {}, {}".format(row.employee, row.employee_name)) 
 
 			if row.overtime_type == "Sunday Overtime (Half Day)":
 				if row.ot_rate_half_day < 1:
-					frappe.throw("Rate for {} cannot be {}".format(row.overtime_type, row.ot_rate_half_day))
+					frappe.throw("Rate for {} cannot be {} for {}, {}".format(row.overtime_type, row.ot_rate_half_day, row.employee, row.employee_name))
 				row.ot_amount = flt(row.ot_rate_half_day,2)
 			elif row.overtime_type == "Sunday Overtime (Full Day)":
 				if row.ot_rate_full_day < 1:
-					frappe.throw("Rate for {} cannot be {}".format(row.overtime_type, row.ot_rate_full_day))
+					frappe.throw("Rate for {} cannot be {} for {}, {}".format(row.overtime_type, row.ot_rate_full_day, row.employee, row.employee_name))
 				row.ot_amount=flt(row.ot_rate_full_day,2)
 			else:
 				if row.ot_rate < 1:
-					frappe.throw("Overtime Rate cannot be {}".format(row.ot_rate))
+					frappe.throw("Overtime Rate for {}, {} cannot be less than 1".format(row.employee, row.employee_name))
 				row.ot_amount = flt(row.ot_rate * row.approved_ot_hrs, 2)
 			total_amount += flt(row.ot_amount,2)
-		self.total_amount = flt(total_amount,2)
+		
+		for row in self.temporary_staff_details:
+			if not row.temp_ot_amount:
+				frappe.throw("OT amount cannot be zero for temporary staff {}, {}".format(row.name_code, row.full_name))
+			if not row.approved_ot_hours:
+				frappe.throw("Please enter approved OT hours for Temporary staff {}, {}".format(row.name_code, row.full_name)) 
+
+			if row.ot_type == "Sunday Overtime (Half Day)":
+				if row.half_day < 1:
+					frappe.throw("Rate for {} cannot be {} for {}, {}".format(row.ot_type, row.half_day, row.name_code, row.full_name))
+				row.temp_ot_amount = flt(row.half_day,2)
+			elif row.ot_type == "Sunday Overtime (Full Day)":
+				if row.full_day < 1:
+					frappe.throw("Rate for {} cannot be {} for {}, {}".format(row.ot_type, row.full_day, row.name_code, row.full_name))
+				row.temp_ot_amount=flt(row.full_day,2)
+			else:
+				if row.rates < 1:
+					frappe.throw("Overtime Rate for temporary employee {}, {} cannot be less than 1".format(row.name_code, row.full_name))
+				row.temp_ot_amount = flt(row.rates * row.approved_ot_hours, 2)
+			temp_total_amount += flt(row.temp_ot_amount,2)
+		self.total_amount = flt(total_amount,2) + flt(temp_total_amount,2)
 
 	def post_overtime_entries(self):
 		for d in self.get("ot_details"):
@@ -92,3 +135,7 @@ class OTUpdateTools(Document):
 @frappe.whitelist()
 def get_rate(employee, overtime_type):
 	return frappe.db.get_value("Employee", employee, ["overtime_normal_rate","sunday_overtime_half_day","sunday_overtime_full_day"])
+
+@frappe.whitelist()
+def get_temp_overtime_rate(staff):
+	return frappe.db.get_value("TES_Temporary Staffs", staff, ["normal_ot_rate","sunday_half_day","sunday_full_day"])
