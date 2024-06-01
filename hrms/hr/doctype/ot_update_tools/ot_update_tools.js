37a971a26d6ade47b6227d15dcc98d5fc7718f61
thukten <thukday@gmail.com>
2024-06-01 19:12:58 +0600

    Thukten

--- hrms/hr/doctype/ot_update_tools/ot_update_tools.js
+++ hrms/hr/doctype/ot_update_tools/ot_update_tools.js
@@ -91,7 +91,7 @@ var post_ot_entries = function(frm){
 		freeze_message: "Posting Entries to Overtime Application.... Please Wait",
 	});
 	window.location.reload();
-	frappe.throw("OT Update Tool is successfully posted ")
+	//frappe.throw("OT Update Tool is successfully posted ")
 }
 
 function get_rate_base_on_overtime_type(frm, cdt, cdn){
