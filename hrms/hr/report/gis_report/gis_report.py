6281e716cd0124a84a04462c02e1e4072eb72676
karma <ktenzleo@gmail.com>
2024-06-21 15:35:38 +0600

    Added payroll I columnD in GIS report and PF report

--- hrms/hr/report/gis_report/gis_report.py
+++ hrms/hr/report/gis_report/gis_report.py
@@ -48,7 +48,9 @@ def get_columns(data):
 		_("Division") + ":Data:120",
 		_("Section") + ":Data:120",
 		_("Year") + "::80",
-		_("Month") + "::80"
+		_("Month") + "::80",
+		_("Pay Roll Entry ID") + ":Link/Payroll Entry:100",
+
 	]
 
 	return columns
@@ -71,21 +73,87 @@ def get_data(filters):
 	# 					group by t1.employee, t3.employee_name, t1.designation, t3.passport_number,  t3.gis_number, 
 	# 			t1.company, t1.branch, t1.department, t1.division, t1.section, t1.fiscal_year, t1.month
 	# 			""" % conditions, filters)
-	data = frappe.db.sql("""select 
-				t1.employee as n, t3.employee_name, t1.designation, t3.passport_number, 
-				t3.date_of_birth, t3.date_of_joining, t3.employee_group, t1.employee_subgroup, t1.gis_number, t1.gis_policy_number,
-				(select sum(t6.amount) from `tabSalary Structure` t5, `tabSalary Slip Item` t4, `tabSalary Detail` t6 where t4.parent = t1.name and t4.salary_structure = t5.name and t6.parent = t5.name and t6.salary_component = 'Basic Pay') as basicpay,
-				sum(case when t2.salary_component = 'GIS' then ifnull(t2.amount,0) else 0 end) as gisamount,
-							t1.company, t1.branch, t1.cost_center, t1.department, t1.division, t1.section,t1.fiscal_year, t1.month
-						from `tabSalary Slip` t1, `tabSalary Detail` t2, `tabEmployee` t3
-						where t1.docstatus = 1 %s
-						and t3.employee = t1.employee
-						and t2.parent = t1.name
-						and t2.salary_component in ('GIS')
-						group by t1.employee, t3.employee_name, t1.designation, t3.passport_number,  t3.gis_number, 
-				t1.company, t1.branch, t1.department, t1.division, t1.section, t1.fiscal_year, t1.month
-				""" % conditions, filters)
+	# data = frappe.db.sql("""select 
+	# 			t1.employee as n, t3.employee_name, t1.designation, t3.passport_number, 
+	# 			t3.date_of_birth, t3.date_of_joining, t3.employee_group, t1.employee_subgroup, t1.gis_number, t1.gis_policy_number,
+	# 			(select sum(t6.amount) from `tabSalary Structure` t5, `tabSalary Slip Item` t4, `tabSalary Detail` t6 where t4.parent = t1.name and t4.salary_structure = t5.name and t6.parent = t5.name and t6.salary_component = 'Basic Pay') as basicpay,
+	# 			sum(case when t2.salary_component = 'GIS' then ifnull(t2.amount,0) else 0 end) as gisamount,
+	# 						t1.company, t1.branch, t1.cost_center, t1.department, t1.division, t1.section,t1.fiscal_year, t1.month
+	# 					from `tabSalary Slip` t1, `tabSalary Detail` t2, `tabEmployee` t3
+	# 					where t1.docstatus = 1 %s
+	# 					and t3.employee = t1.employee
+	# 					and t2.parent = t1.name
+	# 					and t2.salary_component in ('GIS')
+	# 					group by t1.employee, t3.employee_name, t1.designation, t3.passport_number,  t3.gis_number, 
+	# 			t1.company, t1.branch, t1.department, t1.division, t1.section, t1.fiscal_year, t1.month
+	# 			""" % conditions, filters)
+	# return data
+
+
+
+
+	data = frappe.db.sql("""
+		SELECT 
+			t1.employee AS n, 
+			t3.employee_name, 
+			t1.designation, 
+			t3.passport_number, 
+			t3.date_of_birth, 
+			t3.date_of_joining, 
+			t3.employee_group, 
+			t1.employee_subgroup, 
+			t1.gis_number, 
+			t1.gis_policy_number,
+			(SELECT SUM(t6.amount) 
+			FROM `tabSalary Structure` t5, 
+				`tabSalary Slip Item` t4, 
+				`tabSalary Detail` t6 
+			WHERE t4.parent = t1.name 
+			AND t4.salary_structure = t5.name 
+			AND t6.parent = t5.name 
+			AND t6.salary_component = 'Basic Pay') AS basicpay,
+			SUM(CASE 
+				WHEN t2.salary_component = 'GIS' 
+				THEN IFNULL(t2.amount, 0) 
+				ELSE 0 
+				END) AS gisamount,
+			t1.company, 
+			t1.branch, 
+			t1.cost_center, 
+			t1.department, 
+			t1.division, 
+			t1.section,
+			t1.fiscal_year, 
+			t1.month,
+			t1.payroll_entry AS payroll_entry_id  -- Added payroll entry ID
+		FROM 
+			`tabSalary Slip` t1
+		JOIN 
+			`tabSalary Detail` t2 ON t2.parent = t1.name
+		JOIN 
+			`tabEmployee` t3 ON t3.employee = t1.employee
+		LEFT JOIN 
+			`tabPayroll Entry` t7 ON t7.name = t1.payroll_entry  -- Join with Payroll Entry table
+		WHERE 
+			t1.docstatus = 1 %s
+			AND t2.salary_component IN ('GIS')
+		GROUP BY 
+			t1.employee, 
+			t3.employee_name, 
+			t1.designation, 
+			t3.passport_number,  
+			t3.gis_number, 
+			t1.company, 
+			t1.branch, 
+			t1.department, 
+			t1.division, 
+			t1.section, 
+			t1.fiscal_year, 
+			t1.month,
+			t1.payroll_entry
+    """ % conditions, filters)
 	return data
+
 
 def get_conditions(filters):
 	conditions = ""
--- hrms/hr/report/pf_report/pf_report.py
+++ hrms/hr/report/pf_report/pf_report.py
@@ -33,52 +33,36 @@ def get_columns(data):
 				_("CID") + "::120",_("PF Tier") + "::100", _("Basic Pay") + ":Currency:120",_("PF Account#") + "::120",
 				_("Employee PF") + ":Currency:120", _("Employer PF") + ":Currency:120", _("Total") + ":Currency:120",
 				_("Company") + ":Link/Branch:120", _("Cost Center") + ":Link/Cost Center:120", _("Branch") + ":Link/Branch:120", _("Department") + ":Link/Department:120",
-				_("Division") + ":Link/Division:120", _("Section") + ":Link/Section:120", _("Year") + "::80", _("Month") + "::80"
+				_("Division") + ":Link/Division:120", _("Section") + ":Link/Section:120", _("Year") + "::80", _("Month") + "::80", _("Pay Roll Entry ID") + ":Link/Payroll Entry:100"
 	]
 	
 	return columns
 	
 def get_data(filters):
 	conditions, filters = get_conditions(filters)
-
-	# data = frappe.db.sql("""
-	# 		select t1.employee, t3.employee_name, t1.designation, t3.passport_number,t3.pf_tiers,
-	# 				sum(case when t2.salary_component = 'Basic Pay' then ifnull(t2.amount,0) else 0 end) as basicpay,
-	# 				t3.pf_number,
-	# 				sum(case when t2.salary_component = 'PF' then ifnull(t2.amount,0) else 0 end) as employeepf,
-	# 				sum(case when t2.salary_component = 'PF' then ifnull(t2.amount,0) else 0 end) as employerpf,
-	# 				sum(case when t2.salary_component = 'PF' then ifnull(t2.amount,0)*2 else 0 end) as total,
-	# 				t1.company, t1.branch, t1.cost_center, t1.department, t1.division, t1.section,
-	# 				t1.fiscal_year, t1.month
-	# 		from `tabSalary Slip` t1, `tabSalary Detail` t2, `tabEmployee` t3
-	# 		where t1.docstatus = 1 %s
-	# 		and t3.employee = t1.employee
-	# 		and t2.parent = t1.name
-	# 		and t2.salary_component in ('Basic Pay','PF')
-	# 		group by t1.employee, t3.employee_name, t1.designation, t3.passport_number,
-	# 				t3.pf_number, t1.company, t1.branch, t1.department, t1.division, t1.section,
-	# 				t1.fiscal_year, t1.month
-	# 		""" % conditions, filters)
+	 
 	data = frappe.db.sql("""
-			select t1.employee, t3.employee_name, t1.designation, t3.passport_number,t3.pf_tiers,
-					(select sum(t6.amount) from `tabSalary Slip` t5, `tabSalary Detail` t6 where t6.parent = t5.name and t6.salary_component = 'Basic Pay' and t1.name  = t5.name) as basicpay,
-					t3.pf_number,
-					sum(case when t2.salary_component = 'PF' then ifnull(t2.amount,0) else 0 end) as employeepf,
-					sum(case when t2.salary_component = 'PF' then ifnull(t2.amount,0) else 0 end) as employerpf,
-					sum(case when t2.salary_component = 'PF' then ifnull(t2.amount,0)*2 else 0 end) as total,
-					t1.company, t1.branch, t1.cost_center, t1.department, t1.division, t1.section,
-					t1.fiscal_year, t1.month
-			from `tabSalary Slip` t1, `tabSalary Detail` t2, `tabEmployee` t3
-			where t1.docstatus = 1 %s
-			and t3.employee = t1.employee
-			and t2.parent = t1.name
-			and t2.salary_component in ('Basic Pay','PF')
-			group by t1.employee, t3.employee_name, t1.designation, t3.passport_number,
-					t3.pf_number, t1.company, t1.branch, t1.department, t1.division, t1.section,
-					t1.fiscal_year, t1.month
-			""" % conditions, filters)
+			SELECT t1.employee, t3.employee_name, t1.designation, t3.passport_number, t3.pf_tiers,
+				SUM(CASE WHEN t2.salary_component = 'Basic Pay' THEN IFNULL(t2.amount, 0) ELSE 0 END) AS basicpay,
+				t3.pf_number,
+				SUM(CASE WHEN t2.salary_component = 'PF' THEN IFNULL(t2.amount, 0) ELSE 0 END) AS employeepf,
+				SUM(CASE WHEN t2.salary_component = 'PF' THEN IFNULL(t2.amount, 0) ELSE 0 END) AS employerpf,
+				SUM(CASE WHEN t2.salary_component = 'PF' THEN IFNULL(t2.amount, 0) * 2 ELSE 0 END) AS total,
+				t1.company, t1.branch, t1.cost_center, t1.department, t1.division, t1.section,
+				t1.fiscal_year, t1.month, t1.payroll_entry AS payroll_entry_id
+			FROM `tabSalary Slip` t1
+			JOIN `tabSalary Detail` t2 ON t2.parent = t1.name
+			JOIN `tabEmployee` t3 ON t3.employee = t1.employee
+			WHERE t1.docstatus = 1 %s
+			AND t2.salary_component IN ('Basic Pay', 'PF')
+			GROUP BY t1.employee, t3.employee_name, t1.designation, t3.passport_number, t3.pf_number,
+				t1.company, t1.branch, t1.cost_center, t1.department, t1.division, t1.section,
+				t1.fiscal_year, t1.month, t1.payroll_entry
+		""" % conditions, filters)
 
-	return data
+	return data	
+ 
+
 	
 def get_conditions(filters):
 	conditions = ""
