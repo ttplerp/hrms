6426548cdfc14373dd5b2b431c3be03b5b28d9a7
kinleydorji <kd77144@gmail.com>
2024-06-27 16:58:22 +0600

    loan report

--- hrms/payroll/report/loan_report/loan_report.py
+++ hrms/payroll/report/loan_report/loan_report.py
@@ -47,7 +47,7 @@ def get_data(filters):
 			t1.company, t1.cost_center, t1.branch, t1.department, t1.division, t1.section,
 			t1.fiscal_year, t1.month
 		from `tabSalary Slip` t1, `tabSalary Detail` t2, `tabEmployee` t3
-		where t1.docstatus = 1 %s
+		where t1.docstatus = 1 {}
 		and t3.employee = t1.employee
 		and t2.parent = t1.name
 		and t2.parentfield = 'deductions'
@@ -58,7 +58,7 @@ def get_data(filters):
 				where sc.name = t2.salary_component
 			)
 		and t2.reference_type != 'NULL'
-	"""% conditions, filters)
+	""".foramt(conditions))
 	return data
 
 def get_conditions(filters):
@@ -69,9 +69,9 @@ def get_conditions(filters):
 		filters["month"] = month
 		conditions += " and t1.month = %(month)s"
 	
-	if filters.get("fiscal_year"): conditions += " and t1.fiscal_year = %(fiscal_year)s"
-	if filters.get("company"): conditions += " and t1.company = %(company)s"
-	if filters.get("employee"): conditions += " and t1.employee = %(employee)s"
+	if filters.get("fiscal_year"): conditions += " and t1.fiscal_year = {}".format(filters.get("fiscal_year"))
+	if filters.get("company"): conditions += " and t1.company = '{}'".format(filters.get("company"))
+	if filters.get("employee"): conditions += " and t1.employee = '{}'".format(filters.get("employee"))
 	if filters.get("bank"): conditions += "and t2.institution_name = '{0}'".format(filters.bank)
 	if filters.get("cost_center"): conditions += " and exists(select 1 from `tabCost Center` cc where t1.cost_center = cc.name and (cc.parent_cost_center = '{0}' or cc.name = '{0}'))".format(filters.cost_center)
 
