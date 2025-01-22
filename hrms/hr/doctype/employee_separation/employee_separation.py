5fe9f9423adee9d8be470a330ec9b2235b57bbc5
jai <jai>
2025-01-22 15:56:01 +0600

    emp separation py update

--- hrms/hr/doctype/employee_separation/employee_separation.py
+++ hrms/hr/doctype/employee_separation/employee_separation.py
@@ -18,7 +18,7 @@ class EmployeeSeparation(EmployeeBoardingController):
 	def on_submit(self):
 		super(EmployeeSeparation, self).on_submit()
 		if self.reason_for_resignation=="Demise":
-			self.exit_interview =1
+			self.db_set("exit_interview", 1)
 		# notify_workflow_states(self)exit_interview
 
 	# def on_update_after_submit(self):
