51abff85408765fd11e0f5d5c3e3cd7b3cac49f2
Tim <tsheringwangchuky@mail.com>
2025-02-19 12:00:43 +0600

    prepatory leace

--- hrms/hr/doctype/leave_application/leave_application.py
+++ hrms/hr/doctype/leave_application/leave_application.py
@@ -753,11 +753,12 @@ def get_number_of_leave_days(
     
     final=float(no_days)
 
-    if leave_type not in ("Earned Leave", "Casual Leave"):
+    if leave_type not in ("Earned Leave", "Casual Leave","Preparatory leave"):
         return final 
         
     total_days=0
     is_sat=frappe.db.get_value("Holiday List", get_holiday_list_for_employee(employee), "saturday_half")
+ 
     cur_date=from_date
     # print(f"inital: {no_days}")
 
