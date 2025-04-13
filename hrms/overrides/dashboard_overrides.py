5b7367736205fecaf8c0ee0ffb31f71207eb6b59
Nyuethyue <dawanyuethyuetshering111@gmail.com>
2025-04-13 13:13:59 +0600

    mr att

--- hrms/overrides/dashboard_overrides.py
+++ hrms/overrides/dashboard_overrides.py
@@ -20,36 +20,43 @@ def get_dashboard_for_employee(data):
 			{
 				"label": _("Lifecycle"),
 				"items": [
-					"Employee Onboarding",
+					# "Employee Onboarding",
 					"Employee Transfer",
 					"Employee Promotion",
-					"Employee Grievance",
+					# "Employee Grievance",
 				],
 			},
 			{
 				"label": _("Exit"),
 				"items": ["Employee Separation", "Exit Interview", "Full and Final Statement"],
 			},
-			{"label": _("Shift"), "items": ["Shift Request", "Shift Assignment"]},
-			{"label": _("Expense"), "items": ["Expense Claim", "Travel Request", "Employee Advance"]},
-			{"label": _("Benefit"), "items": ["Employee Benefit Application", "Employee Benefit Claim"]},
+			# {"label": _("Shift"), "items": ["Shift Request", "Shift Assignment"]},
+			{"label": _("Expense"), "items": ["Expense Claim", "Employee Advance"]},
+			# {"label": _("Benefit"), "items": ["Employee Benefit Application", "Employee Benefit Claim"]},
 			{
 				"label": _("Payroll"),
 				"items": [
 					"Salary Structure",
 					"Salary Slip",
-					"Additional Salary",
-					"Timesheet",
-					"Employee Incentive",
-					"Retention Bonus",
-					"Bank Account",
+					# "Additional Salary",
+					# "Timesheet",
+					# "Employee Incentive",
+					# "Retention Bonus",
+					# "Bank Account",
 				],
 			},
+			# {
+			# 	"label": _("Training"),
+			# 	"items": ["Training Event", "Training Result", "Training Feedback", "Employee Skill Map"],
+			# },
+			# {"label": _("Evaluation"), "items": ["Appraisal"]},
 			{
-				"label": _("Training"),
-				"items": ["Training Event", "Training Result", "Training Feedback", "Employee Skill Map"],
-			},
-			{"label": _("Evaluation"), "items": ["Appraisal"]},
+				"label": _("Employee Training Details"),
+				"items": [
+					"Employee Training Details"
+				],
+
+			}
 		],
 	}
 
