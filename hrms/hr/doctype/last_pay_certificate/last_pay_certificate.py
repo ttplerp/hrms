fcbdb3668773c10573b24442a79e10e70708c42b
dDendup67 <dendup1998@gmail.com>
2025-05-14 18:00:46 +0600

    Last Pay Certificate

--- hrms/hr/doctype/last_pay_certificate/last_pay_certificate.py
+++ hrms/hr/doctype/last_pay_certificate/last_pay_certificate.py
@@ -1,8 +1,10 @@
 # Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class LastPayCertificate(Document):
-	pass
+	def validate(self):
+		if not self.fixed_basic_pay or self.fixed_basic_pay < 1:
+			frappe.throw("Please provide the Fixed Basic Pay")
