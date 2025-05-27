136e1e885ff497533b73500e2a667c00bbb97061
Sonam-555 <sonamdoji384@gmail.com>
2025-05-27 10:51:27 +0600

    Update PF Report

--- hrms/payroll/report/pf_report/pf_report.js
+++ hrms/payroll/report/pf_report/pf_report.js
@@ -1,3 +1,52 @@
+// // Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and contributors
+// // For license information, please see license.txt
+// /* eslint-disable */
+
+// frappe.query_reports["PF Report"] = {
+// 	"filters": [
+// 		{
+// 			"fieldname": "fiscal_year",
+// 			"label": __("Fiscal Year"),
+// 			"fieldtype": "Link",
+// 			"options": "Fiscal Year",
+// 			"default": frappe.sys_defaults.fiscal_year,
+// 		},
+// 		{
+// 			"fieldname": "month",
+// 			"label": __("Month"),
+// 			"fieldtype": "Select",
+// 			"options": "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec",
+// 			"default": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"][frappe.datetime.str_to_obj(frappe.datetime.get_today()).getMonth()],
+// 		},
+// 		{
+// 			"fieldname": "employee",
+// 			"label": __("Employee"),
+// 			"fieldtype": "Link",
+// 			"options": "Employee"
+// 		},
+// 		{
+// 			"fieldname": "employment_type",
+// 			"label": __("Employment Type"),
+// 			"fieldtype": "Link",
+// 			"options": "Employment Type"
+// 		},
+// 		{
+// 			"fieldname": "cost_center",
+// 			"label": __("Cost Center"),
+// 			"fieldtype": "Link",
+// 			"options": "Cost Center"
+// 		},
+// 		{
+// 			"fieldname": "company",
+// 			"label": __("Company"),
+// 			"fieldtype": "Link",
+// 			"options": "Company",
+// 			"default": frappe.defaults.get_user_default("Company")
+// 		},
+// 	]
+// };
+
+
 // Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 /* eslint-disable */
@@ -15,8 +64,13 @@ frappe.query_reports["PF Report"] = {
 			"fieldname": "month",
 			"label": __("Month"),
 			"fieldtype": "Select",
-			"options": "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec",
-			"default": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"][frappe.datetime.str_to_obj(frappe.datetime.get_today()).getMonth()],
+			"options": "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec",
+			"default": (function () {
+				let month_index = frappe.datetime.str_to_obj(frappe.datetime.get_today()).getMonth(); // 0-based index
+				const month_names = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+									 "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+				return month_names[month_index];
+			})(),
 		},
 		{
 			"fieldname": "employee",
@@ -45,3 +99,4 @@ frappe.query_reports["PF Report"] = {
 		},
 	]
 };
+
