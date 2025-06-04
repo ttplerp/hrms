2488e457de48334e42cb7649fd9d24b630f37aa3
Sonam-555 <sonamdoji384@gmail.com>
2025-06-04 21:34:40 +0600

    Bhutan Broadcasting Services Corporation Enabled

---
+++ hrms/payroll/report/bhutan_broadcasting_services_corporation_enabled/bhutan_broadcasting_services_corporation_enabled.js
@@ -0,0 +1,15 @@
+// Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Bhutan Broadcasting Services Corporation Enabled"] = {
+	"filters": [
+		{
+            "fieldname": "department",
+            "label": __("Department"),
+            "fieldtype": "Link",
+            "options": "Department",
+            "reqd": 1
+        },
+	]
+};
