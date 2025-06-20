238ece4e4787208a349c7bed9a99f2efcc61364e
Sonam-555 <sonamdoji384@gmail.com>
2025-06-20 14:18:32 +0600

    Update Tax and Health

--- hrms/payroll/report/tax_and_health_report/tax_and_health_report.py
+++ hrms/payroll/report/tax_and_health_report/tax_and_health_report.py
@@ -1,3 +1,108 @@
+# # Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and contributors
+# # For license information, please see license.txt
+
+# from __future__ import unicode_literals
+# import frappe
+# from frappe.utils import flt, cstr
+# from frappe import msgprint, _
+
+
+# def execute(filters=None):
+# 	if not filters:
+# 		filters = {}
+# 	columns, data = [], []
+# 	data = get_data(filters)
+# 	if not data:
+# 		return columns, data
+# 	columns =get_columns(data)
+# 	return columns, data
+
+# def get_columns(data):
+# 	columns = [
+# 		_("Employee") + ":Link/Employee:80", 
+# 		_("Employee Name") + "::140", 
+# 		_("Designation") + ":Link/Designation:120",
+# 		_("CID") + "::120", 
+# 		_("TPN#") + "::80",
+# 		_("Basic Salary") + ":Float:120", 
+# 		_("Allowances") + ":Float:120", 
+# 		_("Arrears") + ":Float:120",
+# 		_("Gross Salary(A)") + ":Float:120", 
+# 		_("PF Amount(B)") + ":Float:120", 
+# 		_("GIS Amount(C)") + ":Float:120",
+# 		_("Net Salary(A-(B+C))") + ":Float:140", 
+# 		_("Salary Tax(X)") + ":Float:120", 
+# 		_("Health Contr(Y)") + ":Float:120",
+# 		_("Total(X+Y)") + ":Float:120", 
+# 		_("Company") + ":Link/Company:120", 
+# 		_("Cost Center") + ":Link/Cost Center:120",
+# 		_("Branch") + ":Link/Branch:120", 
+# 		_("Department") + ":Link/Department:120",
+# 		_("Division") + ":Link/Division:120", 
+# 		_("Section") + ":Link/Section:120", 
+# 		_("Year") + "::80", 
+# 		_("Month") + "::80"
+# 	]
+# 	return columns
+
+# def get_data(filters):
+# 	conditions, filters = get_conditions(filters)
+# 	data = frappe.db.sql("""
+# 		select t1.employee, t3.employee_name, t1.designation, t3.passport_number, t3.tpn_number,
+# 			sum(case when t2.salary_component = 'Basic Pay' then ifnull(t2.amount,0) else 0 end) as basicpay,
+# 			sum(case when t2.parentfield = 'earnings'
+# 				 then (case when t2.salary_component = 'Basic Pay' then 0
+# 					    when t2.salary_component = 'Salary  Arrears' then 0
+# 				       else ifnull(t2.amount,0) end)
+# 				 else 0 end) as allowances,
+# 			sum(case when t2.salary_component = 'Salary  Arrears' then ifnull(t2.amount,0) else 0 end) as arrears,
+# 			sum(case when t2.parentfield = 'earnings' then ifnull(t2.amount,0) else 0 end) as grosspay,
+# 			sum(case when t2.salary_component = 'PF' then ifnull(t2.amount,0) else 0 end) as pfamount,
+# 			sum(case when t2.salary_component = 'GIS' then ifnull(t2.amount,0) else 0 end) as gisamount,
+# 			sum(
+# 			   (case when t2.parentfield = 'earnings' then ifnull(t2.amount,0) else 0 end)
+# 			   - (case when t2.salary_component = 'PF' then ifnull(t2.amount,0) else 0 end)
+# 			   - (case when t2.salary_component = 'GIS' then ifnull(t2.amount,0) else 0 end)
+# 			) as netpay,
+# 			sum(case when t2.salary_component = 'Salary Tax' then ifnull(t2.amount,0) else 0 end) as salarytax,
+# 			sum(case when t2.salary_component = 'Health Contribution' then ifnull(t2.amount,0) else 0 end) as healthcont,
+# 			sum(
+# 			   (case when t2.salary_component = 'Salary Tax' then ifnull(t2.amount,0) else 0 end)
+# 			   + (case when t2.salary_component = 'Health Contribution' then ifnull(t2.amount,0) else 0 end)
+# 			) as total,
+# 			t1.company, t1.cost_center, t1.branch, t1.department, t1.division, t1.section,
+# 			t1.fiscal_year, t1.month
+# 		from `tabSalary Slip` t1, `tabSalary Detail` t2, `tabEmployee` t3
+# 		where t1.docstatus = 1 %s
+# 		and t3.employee = t1.employee
+# 		and t2.parent = t1.name
+# 		group by t1.employee, t3.employee_name, t1.designation, t3.passport_number,
+# 			t3.tpn_number, t1.company, t1.branch, t1.department, t1.division, t1.section,
+# 			t1.fiscal_year, t1.month
+# 		""" % conditions, filters)
+
+# 	'''
+# 	if not data:
+# 		msgprint(_("No Data Found for month: ") + cstr(filters.get("month")) + 
+# 			_(" and year: ") + cstr(filters.get("fiscal_year")), raise_exception=1)
+# 	'''
+# 	return data
+
+# def get_conditions(filters):
+# 	conditions = ""
+# 	if filters.get("month"):
+# 		month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", 
+# 			"Dec"].index(filters["month"]) + 1
+# 		filters["month"] = month
+# 		conditions += " and t1.month = %(month)s"
+	
+# 	if filters.get("fiscal_year"): conditions += " and t1.fiscal_year = %(fiscal_year)s"
+# 	if filters.get("company"): conditions += " and t1.company = %(company)s"
+# 	if filters.get("employee"): conditions += " and t1.employee = %(employee)s"
+# 	if filters.get("cost_center"): conditions += " and exists(select 1 from `tabCost Center` cc where t1.cost_center = cc.name and (cc.parent_cost_center = '{0}' or cc.name = '{0}'))".format(filters.cost_center)
+# 	return conditions, filters
+
+
 # Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
@@ -6,7 +111,6 @@ import frappe
 from frappe.utils import flt, cstr
 from frappe import msgprint, _
 
-
 def execute(filters=None):
 	if not filters:
 		filters = {}
@@ -14,11 +118,11 @@ def execute(filters=None):
 	data = get_data(filters)
 	if not data:
 		return columns, data
-	columns =get_columns(data)
+	columns = get_columns(data)
 	return columns, data
 
 def get_columns(data):
-	columns = [
+	return [
 		_("Employee") + ":Link/Employee:80", 
 		_("Employee Name") + "::140", 
 		_("Designation") + ":Link/Designation:120",
@@ -43,61 +147,83 @@ def get_columns(data):
 		_("Year") + "::80", 
 		_("Month") + "::80"
 	]
-	return columns
 
 def get_data(filters):
 	conditions, filters = get_conditions(filters)
-	data = frappe.db.sql("""
-		select t1.employee, t3.employee_name, t1.designation, t3.passport_number, t3.tpn_number,
-			sum(case when t2.salary_component = 'Basic Pay' then ifnull(t2.amount,0) else 0 end) as basicpay,
-			sum(case when t2.parentfield = 'earnings'
-				 then (case when t2.salary_component = 'Basic Pay' then 0
-					    when t2.salary_component = 'Salary  Arrears' then 0
-				       else ifnull(t2.amount,0) end)
-				 else 0 end) as allowances,
-			sum(case when t2.salary_component = 'Salary  Arrears' then ifnull(t2.amount,0) else 0 end) as arrears,
-			sum(case when t2.parentfield = 'earnings' then ifnull(t2.amount,0) else 0 end) as grosspay,
-			sum(case when t2.salary_component = 'PF' then ifnull(t2.amount,0) else 0 end) as pfamount,
-			sum(case when t2.salary_component = 'GIS' then ifnull(t2.amount,0) else 0 end) as gisamount,
-			sum(
-			   (case when t2.parentfield = 'earnings' then ifnull(t2.amount,0) else 0 end)
-			   - (case when t2.salary_component = 'PF' then ifnull(t2.amount,0) else 0 end)
-			   - (case when t2.salary_component = 'GIS' then ifnull(t2.amount,0) else 0 end)
-			) as netpay,
-			sum(case when t2.salary_component = 'Salary Tax' then ifnull(t2.amount,0) else 0 end) as salarytax,
-			sum(case when t2.salary_component = 'Health Contribution' then ifnull(t2.amount,0) else 0 end) as healthcont,
-			sum(
-			   (case when t2.salary_component = 'Salary Tax' then ifnull(t2.amount,0) else 0 end)
-			   + (case when t2.salary_component = 'Health Contribution' then ifnull(t2.amount,0) else 0 end)
-			) as total,
-			t1.company, t1.cost_center, t1.branch, t1.department, t1.division, t1.section,
-			t1.fiscal_year, t1.month
-		from `tabSalary Slip` t1, `tabSalary Detail` t2, `tabEmployee` t3
-		where t1.docstatus = 1 %s
-		and t3.employee = t1.employee
-		and t2.parent = t1.name
-		group by t1.employee, t3.employee_name, t1.designation, t3.passport_number,
-			t3.tpn_number, t1.company, t1.branch, t1.department, t1.division, t1.section,
-			t1.fiscal_year, t1.month
-		""" % conditions, filters)
 
-	'''
-	if not data:
-		msgprint(_("No Data Found for month: ") + cstr(filters.get("month")) + 
-			_(" and year: ") + cstr(filters.get("fiscal_year")), raise_exception=1)
-	'''
+	query = f"""
+		SELECT
+			t1.employee,
+			t3.employee_name,
+			t1.designation,
+			t3.passport_number,
+			t3.tpn_number,
+			SUM(CASE WHEN t2.salary_component = 'Basic Pay' THEN IFNULL(t2.amount, 0) ELSE 0 END) AS basicpay,
+			SUM(CASE
+					WHEN t2.parentfield = 'earnings' THEN
+						CASE
+							WHEN t2.salary_component = 'Basic Pay' THEN 0
+							WHEN t2.salary_component = 'Salary  Arrears' THEN 0
+							ELSE IFNULL(t2.amount, 0)
+						END
+					ELSE 0
+				END) AS allowances,
+			SUM(CASE WHEN t2.salary_component = 'Salary  Arrears' THEN IFNULL(t2.amount, 0) ELSE 0 END) AS arrears,
+			SUM(CASE WHEN t2.parentfield = 'earnings' THEN IFNULL(t2.amount, 0) ELSE 0 END) AS grosspay,
+			SUM(CASE WHEN t2.salary_component = 'PF' THEN IFNULL(t2.amount, 0) ELSE 0 END) AS pfamount,
+			SUM(CASE WHEN t2.salary_component = 'GIS' THEN IFNULL(t2.amount, 0) ELSE 0 END) AS gisamount,
+			SUM(
+				CASE WHEN t2.parentfield = 'earnings' THEN IFNULL(t2.amount, 0) ELSE 0 END
+				- CASE WHEN t2.salary_component = 'PF' THEN IFNULL(t2.amount, 0) ELSE 0 END
+				- CASE WHEN t2.salary_component = 'GIS' THEN IFNULL(t2.amount, 0) ELSE 0 END
+			) AS netpay,
+			SUM(CASE WHEN t2.salary_component = 'Salary Tax' THEN IFNULL(t2.amount, 0) ELSE 0 END) AS salarytax,
+			SUM(CASE WHEN t2.salary_component = 'Health Contribution' THEN IFNULL(t2.amount, 0) ELSE 0 END) AS healthcont,
+			SUM(
+				CASE WHEN t2.salary_component = 'Salary Tax' THEN IFNULL(t2.amount, 0) ELSE 0 END +
+				CASE WHEN t2.salary_component = 'Health Contribution' THEN IFNULL(t2.amount, 0) ELSE 0 END
+			) AS total,
+			t1.company,
+			t1.cost_center,
+			t1.branch,
+			t1.department,
+			t1.division,
+			t1.section,
+			t1.fiscal_year,
+			t1.month
+		FROM
+			`tabSalary Slip` t1
+		JOIN `tabSalary Detail` t2 ON t2.parent = t1.name
+		JOIN `tabEmployee` t3 ON t3.name = t1.employee
+		WHERE
+			t1.docstatus = 1 {conditions}
+		GROUP BY
+			t1.employee, t3.employee_name, t1.designation, t3.passport_number,
+			t3.tpn_number, t1.company, t1.cost_center, t1.branch, t1.department,
+			t1.division, t1.section, t1.fiscal_year, t1.month
+	"""
+
+	data = frappe.db.sql(query, filters, as_list=1)
+
+	
+
 	return data
 
 def get_conditions(filters):
 	conditions = ""
 	if filters.get("month"):
-		month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", 
-			"Dec"].index(filters["month"]) + 1
-		filters["month"] = month
-		conditions += " and t1.month = %(month)s"
-	
-	if filters.get("fiscal_year"): conditions += " and t1.fiscal_year = %(fiscal_year)s"
-	if filters.get("company"): conditions += " and t1.company = %(company)s"
-	if filters.get("employee"): conditions += " and t1.employee = %(employee)s"
-	if filters.get("cost_center"): conditions += " and exists(select 1 from `tabCost Center` cc where t1.cost_center = cc.name and (cc.parent_cost_center = '{0}' or cc.name = '{0}'))".format(filters.cost_center)
+		month_index = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"].index(filters["month"]) + 1
+		filters["month"] = month_index
+		conditions += " AND t1.month = %(month)s"
+	if filters.get("fiscal_year"):
+		conditions += " AND t1.fiscal_year = %(fiscal_year)s"
+	if filters.get("company"):
+		conditions += " AND t1.company = %(company)s"
+	if filters.get("employee"):
+		conditions += " AND t1.employee = %(employee)s"
+	if filters.get("cost_center"):
+		conditions += """ AND EXISTS (
+			SELECT 1 FROM `tabCost Center` cc
+			WHERE t1.cost_center = cc.name AND (cc.parent_cost_center = %(cost_center)s OR cc.name = %(cost_center)s)
+		)"""
 	return conditions, filters
