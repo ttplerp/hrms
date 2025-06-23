1a8061bbfec7ccdf94d0e410b3e91dea25b4244f
kinleydorji <kd77144@gmail.com>
2025-06-23 12:02:06 +0600

    employee benefits

--- hrms/hr/doctype/employee_benefits/employee_benefits.py
+++ hrms/hr/doctype/employee_benefits/employee_benefits.py
@@ -94,9 +94,10 @@ class EmployeeBenefits(Document):
 
 	def update_reference(self):
 		if self.employee_separation_id:
-			id = frappe.get_doc("Employee Separation",self.employee_separation_id)
-			id.employee_benefits_status = "Claimed"
-			id.save()
+			# id = frappe.get_doc("Employee Separation",self.employee_separation_id)
+			# id.employee_benefits_status = "Claimed"
+			# id.save()
+			frappe.db.set_value("Employee Separation",self.employee_separation_id,"employee_benefits_status","Claimed")
 		elif self.employee_transfer_id:
 			id = frappe.get_doc("Employee Transfer",self.employee_transfer_id)
 			id.employee_benefits_status = "Claimed"
