7cdfa81df1c35be7515bd83c4bfc315624d5ed2b
Sonam-555 <sonamdoji384@gmail.com>
2025-09-18 12:07:31 +0600

    Update SWS Loan Report

---
+++ hrms/payroll/report/sws_loan_report/sws_loan_report.js
@@ -0,0 +1,15 @@
+// Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["SWS Loan Report"] = {
+	"filters": [
+		{
+			"fieldname": "employee",
+			"label": __("Employee"),
+			"fieldtype": "Link",
+			"options": "Employee",
+			"width": "150px"
+		}
+	]
+};
---
+++ hrms/payroll/report/sws_loan_report/sws_loan_report.py
@@ -0,0 +1,82 @@
+# Copyright (c) 2025, Frappe Technologies Pvt. Ltd.
+# For license information, please see license.txt
+
+import frappe
+
+def execute(filters=None):
+    columns = get_columns()
+    data = get_data(filters)
+    return columns, data
+
+
+def get_columns():
+    return [
+        {
+            "label": "Employee",
+            "fieldname": "employee",
+            "fieldtype": "Link",
+            "options": "Employee",
+            "width": 150
+        },
+        {
+            "label": "Employee Name",
+            "fieldname": "employee_name",
+            "fieldtype": "Data",
+            "width": 200
+        },
+        {
+            "label": "Designation",
+            "fieldname": "designation",
+            "fieldtype": "Link",
+            "options": "Designation",
+            "width": 180
+        },
+        {
+            "label": "Department",
+            "fieldname": "department",
+            "fieldtype": "Link",
+            "options": "Department",
+            "width": 180
+        },
+        {
+            "label": "Salary Component",
+            "fieldname": "salary_component",
+            "fieldtype": "Link",
+            "options": "Salary Component",
+            "width": 200
+        },
+        {
+            "label": "Deduction Amount",
+            "fieldname": "amount",
+            "fieldtype": "Currency",
+            "width": 150
+        },
+    ]
+
+
+def get_data(filters):
+    conditions = ""
+    values = {}
+
+    if filters.get("employee"):
+        conditions += " AND ss.employee = %(employee)s"
+        values["employee"] = filters.get("employee")
+
+    query = f"""
+        SELECT 
+            ss.employee,
+            ss.employee_name,
+            ss.designation,
+            ss.department,
+            sd.salary_component,
+            sd.amount,
+            ss.name as salary_slip
+        FROM `tabSalary Slip` ss
+        INNER JOIN `tabSalary Detail` sd 
+            ON ss.name = sd.parent
+        WHERE sd.salary_component = 'SWS Loan Deduction'
+          AND ss.docstatus = 1
+          {conditions}
+    """
+
+    return frappe.db.sql(query, values, as_dict=1)
