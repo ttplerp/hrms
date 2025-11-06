96ea58a56c717156f47974d625eac8eef97d8fd2
jai <jai>
2025-11-06 12:19:39 +0600

    auditor role add to leave encashment

--- hrms/hr/doctype/employee_advance/employee_advance.py
+++ hrms/hr/doctype/employee_advance/employee_advance.py
@@ -669,7 +669,7 @@ def get_permission_query_conditions(user):
 
 	if user == "Administrator":
 		return
-	if "HR User" in user_roles or "HR Manager" in user_roles or "Accounts User" in user_roles or "Accounts Manager" in user_roles or "System Manager" in user_roles:
+	if "HR User" in user_roles or "HR Manager" in user_roles or "Accounts User" in user_roles or "Accounts Manager" in user_roles or "System Manager" in user_roles or "Auditor" in user_roles:
 		return
 
 	return """(
