021531592a2d593091c9cbd7cd202b7c9d7a58cd
tw <tsheringwangchuky@gmail.com>
2025-12-24 10:37:14 +0600

    Workforce Requistion Push to PRD

--- hrms/hr/doctype/work_force_requisition/work_force_requisition.js
+++ hrms/hr/doctype/work_force_requisition/work_force_requisition.js
@@ -1,15 +1,35 @@
-// Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
-// For license information, please see license.txt
 
 frappe.ui.form.on('Work force Requisition', {
-	requesting_branch: function(frm){
-	
-		if (frm.doc.requesting_branch === "VCSC"){
-			frm.set_value("request_from", "Corporate Office");
-		}
-		else{
-			frm.set_value("request_from","VCSC")
-		}
-
-	}
+	refresh: function(frm) {
+        if (frm.doc.requested_by) {
+            fetch_approver(frm);
+        }
+    },
+    requesting_branch: function(frm) {
+        if (frm.doc.requesting_branch === "VCSC") {
+            frm.set_value("request_from", "Corporate Office");
+        } else {
+            frm.set_value("request_from", "VCSC");
+        }
+    },
+	requested_by: function(frm) {
+        if (!frm.doc.requested_by) return;
+        fetch_approver(frm);
+    },
 });
+
+function fetch_approver(frm) {
+    frappe.call({
+        method: "hrms.hr.doctype.work_force_requisition.work_force_requisition.get_workforce_requisiton_approver",
+        args: { user_id: frm.doc.requested_by }, 
+        callback: function(r) {
+            if (r.message) {
+                frm.set_value("approver", r.message.approver);
+                frm.set_value("approver_name", r.message.approver_name);
+                frm.set_value("approver_designation", r.message.approver_designation);
+            } else {
+                frappe.msgprint("No approver found for this employee.");
+            }
+        }
+    });
+}
