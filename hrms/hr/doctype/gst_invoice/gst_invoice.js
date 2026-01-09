9182538cc2ff360d2bb333d62870853fd6dc1434
jai <jai>
2026-01-09 11:09:40 +0600

    gst invoice, calc gst and actual fee

--- hrms/hr/doctype/gst_invoice/gst_invoice.js
+++ hrms/hr/doctype/gst_invoice/gst_invoice.js
@@ -3,6 +3,32 @@
 
 frappe.ui.form.on('GST Invoice', {
 	refresh: function(frm) {
+		if (frm.doc.cbs_status !== 'SUCCESS' && frm.doc.docstatus === 1) {
+			frm.add_custom_button(__('Make CBS Entry'), function() {
+				frm.call('before_submit').then(() => {
+					frm.reload_doc();
+				});
+			});
+		}
+	},
+	
+});
 
+frappe.ui.form.on('GST Invoice Item', {
+	total_amount: function(frm, cdt, cdn) {
+		let row = locals[cdt][cdn];
+		if (row.total_amount) {
+			// calculate 5% gst and actual amount
+			// actual amount = total_amount / 1.05; derived from, total_amount = actual_amount (1 + gst_rate)
+			let actual_amount = row.total_amount / 1.05;
+			let gst_amount = row.total_amount - actual_amount;
+
+			frappe.model.set_value(cdt, cdn, 'fee_charges', actual_amount);
+			frappe.model.set_value(cdt, cdn, 'gst', gst_amount);
+		} else {
+			frappe.model.set_value(cdt, cdn, 'fee_charges', 0);
+			frappe.model.set_value(cdt, cdn, 'gst', 0);
+		}
 	}
+
 });
--- hrms/hr/doctype/gst_invoice/gst_invoice.py
+++ hrms/hr/doctype/gst_invoice/gst_invoice.py
@@ -1,8 +1,25 @@
 # Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
+from erpnext.integrations.bank_api import gst_entry_adjustment
 
 class GSTInvoice(Document):
-	pass
+	def validate(self):
+		if not self.item:
+			frappe.throw("Please enter GST Item")
+	
+	@frappe.whitelist()
+	def before_submit(self):
+		if not self.cbs_status or self.cbs_status != "Success":
+			response, msg = gst_entry_adjustment(self.name)
+			self.cbs_response = response
+			self.cbs_status = msg
+	
+	def on_submit(self):
+		pass
+	
+	def before_cancel(self):
+		frappe.throw("Cancel is not permitted")
