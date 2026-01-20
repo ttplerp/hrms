e11957d882ae3062326d8f79b67679750123d424
jai <jai>
2026-01-20 12:01:51 +0600

    leave application update

--- hrms/hr/doctype/leave_application/leave_application.py
+++ hrms/hr/doctype/leave_application/leave_application.py
@@ -145,7 +145,8 @@ class LeaveApplication(Document):
                 _("Only Leave Applications with status 'Approved' and 'Rejected' can be submitted")
             )
         self.validate_back_dated_application()
-        self.update_attendance()
+        if self.leave_type not in ("Extraordinary Leave", "Study Leave", "Medical Leave", "Maternity Leave"):
+            self.update_attendance()
         self.notify_employee()
         self.create_leave_ledger_entry()
         self.reload()
@@ -812,7 +813,7 @@ def get_number_of_leave_days(
         for holiday in frappe.db.sql("select * from `tabHoliday` where parent='{}'".format(get_holiday_list_for_employee(employee)), as_dict=1):
             hol_date=getdate(cur_date)
             if holiday.holiday_date==hol_date:
-                if holiday.holiday_date.weekday()==5 and ("saturday" in holiday.description.lower()):
+                if holiday.holiday_date.weekday()==5 and ("saturday" in holiday.description.lower()) and hol_date < getdate('2026-01-17'):
                     flag_half_day = 1
                     #print(f"Saturday: date {cur_date} holiday {holiday.holiday_date}" )
                     final-=0.5  
@@ -1379,7 +1380,7 @@ def get_permission_query_conditions(user):
 
     if user == "Administrator":
         return
-    if "HR User" in user_roles or "HR Manager" in user_roles:
+    if "HR User" in user_roles or "HR Manager" in user_roles or "Auditor" in user_roles:
         return
 
     return """(
