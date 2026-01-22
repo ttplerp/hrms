689696a66cb1da0aaf7a919242c8ad0e41e85f68
rinzin <rinzinn1996@gmail.com>
2026-01-22 11:58:13 +0600

    updated

--- hrms/payroll/doctype/salary_structure/salary_structure.py
+++ hrms/payroll/doctype/salary_structure/salary_structure.py
@@ -345,7 +345,7 @@ class SalaryStructure(Document):
 
 			# Calculating Salary Tax
 			if ed == 'deductions':
-				calc_amt = get_salary_tax(math.floor(flt(total_earning)-flt(pf_amt)-flt(gis_amt)-(comm_allowance*0.5)))
+				calc_amt = get_salary_tax(math.floor(flt(total_earning)-flt(total_earning*0.15)))
 				calc_amt = roundoff(calc_amt)
 				total_deduction += calc_amt
 				calc_map.append({'salary_component': 'Salary Tax', 'amount': flt(calc_amt)})
@@ -569,7 +569,7 @@ def make_salary_slip(source_name, target_doc=None, calc_days={}):
 			else:
 				if d['salary_component'] == 'Salary Tax':
 					if not tax_included:
-						tax_amt = get_salary_tax(math.floor(flt(gross_amt) - flt(gis_amt) - flt(pf_amt) - (flt(comm_amt) * 0.5)))
+						tax_amt = get_salary_tax(math.floor(flt(gross_amt) - flt(gross_amt*0.15)))
 						tax_amt = roundoff(tax_amt)
 						d['amount'] = flt(tax_amt)
 						tax_included = 1
