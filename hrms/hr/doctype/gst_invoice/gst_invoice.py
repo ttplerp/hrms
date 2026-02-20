742668e93aa2fdee9390179b938c9610eb74b49a
jai <jai>
2026-02-20 16:05:34 +0600

    gst invoice on cancel update

--- hrms/hr/doctype/gst_invoice/gst_invoice.py
+++ hrms/hr/doctype/gst_invoice/gst_invoice.py
@@ -20,7 +20,9 @@ class GSTInvoice(Document):
 		pass
 	
 	def before_cancel(self):
-		if "Accounts User" not in frappe.get_roles(frappe.session.user) or "GST Invoice User" not in frappe.get_roles(frappe.session.user):
+		if "Accounts User" in frappe.get_roles(frappe.session.user) or "GST Invoice User" in frappe.get_roles(frappe.session.user):
+			return
+		else:
 			frappe.throw("Cancel is not permitted")
 
 	@frappe.whitelist()
