bf1ba3c9f038ca36874d095f4bf27747c584e0e3
jai <jai>
2026-03-30 09:22:11 +0600

    gst service type update

--- hrms/hr/doctype/gst_service_type/gst_service_type.py
+++ hrms/hr/doctype/gst_service_type/gst_service_type.py
@@ -1,8 +1,12 @@
 # Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class GSTServiceType(Document):
-	pass
+	def validate(self):
+		if not self.require_adjustment:
+			self.service_gl_account = ''
+			self.gst_gl_account = ''
+	
